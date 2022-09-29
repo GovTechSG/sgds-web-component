e227868b160489b10182313fb44440587027e980
clukhei <kheiboardwarrior@gmail.com>
2022-09-29 12:22:47 +0800

    docs(mainnav): add api table

--- src/MainNav/mainnav-element.ts
+++ src/MainNav/mainnav-element.ts
@@ -1,11 +1,11 @@
-import { LitElement, html, PropertyValueMap } from "lit";
-import { customElement, property, query, state } from "lit/decorators.js";
+import { LitElement, html } from "lit";
+import { customElement, property, state } from "lit/decorators.js";
 import styles from "./mainnav.scss";
-import { Collapse } from "bootstrap";
+import { Collapse, Offcanvas } from "bootstrap";
 import { ref, createRef, Ref } from "lit/directives/ref.js";
 import genId from "../utils/generateId";
 import { ResizeController } from "@lit-labs/observers/resize_controller.js";
-// >576px, >768px, >992, >1200, >1400
+
 type Size = "sm" | "md" | "lg" | "xl" | "xxl" | "always" | "never";
 export const SM_BREAKPOINT = 576;
 export const MD_BREAKPOINT = 768;
@@ -24,14 +24,22 @@ const SIZES = {
 };
 @customElement("mainnav-element")
 export class MainNavElement extends LitElement {
-  @query(".navbar-toggler") togglerElement: HTMLButtonElement;
-
   static styles = styles;
+
+  constructor() {
+    super();
+  }
 
   private myCollapse: Ref<HTMLElement> = createRef();
   private bsCollapse: Collapse = null;
+  // TODO: stylings and slots are incomplete for offcanvas mode 
+  private myOffcanvas: Ref<HTMLElement> = createRef();
+  private bsOffcanvas: Offcanvas = null;
 
   private _onClickButton() {
+    if (this.mode === "offcanvas") {
+      return this.bsOffcanvas.toggle();
+    }
     this.bsCollapse.toggle();
   }
 
@@ -44,41 +52,52 @@ export class MainNavElement extends LitElement {
   @property()
   expand: Size = "lg";
 
-  @property({ type: String })
-  navClasses = "";
-
   @state()
   expanded: Boolean = false;
 
+  @property({ type: String })
+  mode: "offcanvas" | "default" = "default";
+
   firstUpdated() {
-    this.bsCollapse = new Collapse(this.myCollapse.value, {
-      toggle: false,
-    });
-    this.myCollapse.value.addEventListener("show.bs.collapse", () => {
-      console.log("show");
-      this.expanded = true;
-    });
-    this.myCollapse.value.addEventListener("shown.bs.collapse", () => {
-      console.log("shown");
-      this.expanded = true;
-    });
-    this.myCollapse.value.addEventListener("hide.bs.collapse", () => {
-      console.log("hide");
-      this.expanded = false;
-    });
-    this.myCollapse.value.addEventListener("hidden.bs.collapse", () => {
-      console.log("hidden");
-      this.expanded = false;
-    });
+    if (this.mode === "default") {
+      this.bsCollapse = new Collapse(this.myCollapse.value, {
+        toggle: false,
+      });
+      this.myCollapse.value.addEventListener("show.bs.collapse", () => {
+        this.expanded = true;
+      });
+      this.myCollapse.value.addEventListener("shown.bs.collapse", () => {
+        this.expanded = true;
+      });
+      this.myCollapse.value.addEventListener("hide.bs.collapse", () => {
+        this.expanded = false;
+      });
+      this.myCollapse.value.addEventListener("hidden.bs.collapse", () => {
+        this.expanded = false;
+      });
+    }
+
+    if (this.mode === "offcanvas") {
+      this.bsOffcanvas = new Offcanvas(this.myOffcanvas.value);
+      //add esc keyboard event for bsOffcanvas
+      this.addEventListener("keydown", (ev) => {
+        if (ev.key === "Escape") {
+          this.bsOffcanvas.hide();
+        }
+      });
+    }
   }
+  // Detects changes in element size and runs update
   _resizeController = new ResizeController(this, {});
 
   render() {
     const breakpointReached = this.offsetWidth < SIZES[this.expand.toString()];
+    const collapseClass = "collapse navbar-collapse order-4";
+    const offcanvasClass = "offcanvas offcanvas-start";
     return html`
       <nav
         class="sgds navbar navbar-light
-        ${this._expandClass()} ${this.navClasses}"
+        ${this._expandClass()}"
       >
         <a class="navbar-brand me-auto order-1" href=${this.brandHref}>
           <slot name="brand"></slot>
@@ -98,8 +117,10 @@ export class MainNavElement extends LitElement {
           <span class="navbar-toggler-icon"></span>
         </button>
         <div
-          class="collapse navbar-collapse order-4"
-          ${ref(this.myCollapse)}
+          class=${this.mode === "default" ? collapseClass : offcanvasClass}
+          ${this.mode === "default"
+            ? ref(this.myCollapse)
+            : ref(this.myOffcanvas)}
           id=${this.collapseId}
         >
           <ul class="navbar-nav">
