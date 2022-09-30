0268080cac0dac6f674ba2db838805ca8f0ada91
rav3 <rav3tey@gmail.com>
2022-09-30 12:32:53 +0800

    feat(button): add button component

--- src/Button/button-element.ts
+++ src/Button/button-element.ts
@@ -1,7 +1,6 @@
 import { LitElement} from "lit";
 import { customElement, property } from "lit/decorators.js";
 import { html, literal } from 'lit/static-html.js';
-import { classMap } from 'lit/directives/class-map.js';
 import { ifDefined } from 'lit/directives/if-defined.js';
 import styles from "./button-element.scss";
 
@@ -10,40 +9,15 @@ export class ButtonElement extends LitElement {
   static styles = styles;
 
   /** The button's variant. */
-  @property({ reflect: true }) 
-  variant: 
-  'default' | 
-  'primary' | 
-  'outline-primary' | 
-  'secondary' | 
-  'outline-secondary' | 
-  'success' | 
-  'outline-success' | 
-  'warning' | 
-  'outline-warning' | 
-  'danger' | 
-  'outline-danger' | 
-  'info' | 
-  'outline-info' | 
-  'light' | 
-  'outline-light' | 
-  'dark' | 
-  'outline-dark' | 
-  'text' | 
-  'link' ;
+  @property({ reflect: true }) variant = "primary";
+  
+  @property({ reflect: true }) class? : string;
 
-
-  /** Draws an outlined button. */
   @property({ reflect: true }) size :'default' | 'sm' | 'lg' ;
 
-  /** Draws an outlined button. */
-  @property({ type: Boolean, reflect: true }) block = false ;
-
-  /** Draws an outlined button. */
   @property({ type: Boolean, reflect: true }) disabled = false ;
 
-  /** Draws an outlined button. */
-  @property({ reflect: true }) type: 'button' | 'submit' | 'reset' = 'button' ;
+  @property() type: 'button' | 'submit' | 'reset' = 'button' ;
 
   /** When set, the underlying button will be rendered as an `<a>` with this `href` instead of a `<button>`. */
   @property() href?: string;
@@ -52,40 +26,22 @@ export class ButtonElement extends LitElement {
   @property() target?: '_blank' | '_parent' | '_self' | '_top';
 
   /** Tells the browser to download the linked file as this filename. Only used when `href` is set. */
-  @property() download?: string;
-
+  @property({ reflect: true }) download?: string;
 
   render() {
     const isLink = this.href ? true : false;
     const tag = isLink ? literal`a` : literal`button`;
+
     return html`
       <${tag}
-        part="base"
-        class="sgds btn${classMap({
-          "btn-sm" : this.size === "sm",
-          "btn-lg" : this.size === "lg",
-          "btn-primary": this.variant === "primary",
-          "btn-outline-primary": this.variant === "outline-primary",
-          "btn-secondary": this.variant === "secondary",
-          "btn-outline-secondary": this.variant === "outline-secondary",
-          "btn-success": this.variant === "success",
-          "btn-outline-success": this.variant === "outline-success",
-          "btn-warning": this.variant === "warning",
-          "btn-outline-warning": this.variant === "outline-warning",
-          "btn-danger": this.variant === "danger",
-          "btn-outline-danger": this.variant === "outline-danger",
-          "btn-info": this.variant === "info",
-          "btn-outline-info": this.variant === "outline-info",
-          "btn-text": this.variant === "text",
-          "btn-link": this.variant === "link",
-        })}"
+        class="btn btn-${this.variant} ${this.class}"
         ?disabled=${ifDefined(isLink ? undefined : this.disabled)}
         type=${ifDefined(isLink ? undefined : this.type)}
         href=${ifDefined(isLink ? this.href : undefined)}
         target=${ifDefined(isLink ? this.target : undefined)}
         download=${ifDefined(isLink ? this.download : undefined)}
-        rel=${ifDefined(isLink && this.target ? 'noreferrer noopener' : undefined)}
-        role=${ifDefined(isLink ? undefined : 'button')}
+        rel=${ifDefined(isLink && this.target === "_blank" ? 'noreferrer noopener' : undefined)}
+        role=${ifDefined(isLink ? 'button': undefined )}
         aria-disabled=${this.disabled ? 'true' : 'false'}
         tabindex=${this.disabled ? '-1' : '0'}
       >
