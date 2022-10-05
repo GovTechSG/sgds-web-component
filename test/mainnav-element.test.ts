0ee38f9cedd1d439d29d09f30aaf489aaf6eaa86
clukhei <kheiboardwarrior@gmail.com>
2022-10-05 10:28:59 +0800

    chore(mainnav): change name

--- test/mainnav-element.test.ts
+++ test/mainnav-element.test.ts
@@ -1,5 +1,5 @@
-import { SgdsMainnavItem, SgdsMainnav } from "../src/Mainnav";
-import "../src/Mainnav";
+import { SgdsMainnavItem, SgdsMainnav } from "../src/MainNavsdf";
+import "../src/MainNavsdf";
 import {
   fixture,
   assert,
