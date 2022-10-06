3a36a69cad96e843ab5528077151cb2a7f43b72d
hafizi <61571184+hafiz-ism@users.noreply.github.com>
2022-10-06 11:48:28 +0800

    chore(): update unit tests

--- test/sgds-checkbox.test.ts
+++ test/sgds-checkbox.test.ts
@@ -17,22 +17,10 @@ describe("<sgds-checkbox>", () => {
     assert.instanceOf(el, SgdsCheckbox);
   });
 
-  it("should have aria-checked false by default", async () => {
-    const el = await fixture(html`<sgds-checkbox></sgds-checkbox>`);
-    const checkbox = el.shadowRoot?.querySelector("input");
-    expect(checkbox?.getAttribute("aria-checked")).to.equal(false);
-  });
-
   it("should be disabled with the disabled attribute", async () => {
     const el = await fixture(html`<sgds-checkbox disabled></sgds-checkbox>`);
     const checkbox = el.shadowRoot?.querySelector("input");
     expect(checkbox?.disabled).to.be.true;
-  });
-
-  it("should support inline", async () => {
-    const el = await fixture(html`<sgds-checkbox isInline></sgds-checkbox>`);
-    const container = el.shadowRoot?.querySelector("label");
-    expect(container?.classList.value).to.contain("form-check-inline");
   });
 
   it("should click the inner input", async () => {
@@ -49,6 +37,32 @@ describe("<sgds-checkbox>", () => {
 
     expect(clickSpy.called).to.equal(true);
     expect(el.checked).to.equal(true);
+  });
+
+  it("should have input aria-checked false by default", async () => {
+    const el = await fixture<SgdsCheckbox>(
+      html`<sgds-checkbox></sgds-checkbox>`
+    );
+    const checkbox = el.shadowRoot!.querySelector("input");
+
+    expect(checkbox!).to.have.attribute("aria-checked", "false");
+  });
+
+  it("should show aria-checked to be true when checked is true", async () => {
+    const el = await fixture<SgdsCheckbox>(
+      html`<sgds-checkbox></sgds-checkbox>`
+    );
+    const checkbox = el.shadowRoot!.querySelector("input")!;
+    const clickSpy = sinon.spy();
+
+    checkbox.addEventListener("click", clickSpy, { once: true });
+
+    el.click();
+    await el.updateComplete;
+
+    expect(clickSpy.called).to.equal(true);
+    expect(el.checked).to.equal(true);
+    expect(checkbox).to.have.attribute("aria-checked", "true");
   });
 
   //   it("should focus the inner input", async () => {
