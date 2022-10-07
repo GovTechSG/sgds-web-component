82d92dad278037802155fb495a0b8356e3ccf051
clukhei <kheiboardwarrior@gmail.com>
2022-10-07 11:48:56 +0800

    fix(mainnav): change to svg element instead of svg link for hamburger menu

--- test/mainnav.test.ts
+++ test/mainnav.test.ts
@@ -41,8 +41,19 @@ describe("sgds-mainnav", () => {
          class="navbar-toggler order-3"
          type="button"
        >
-         <span class="navbar-toggler-icon">
-         </span>
+       <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="30"
+            height="30"
+            fill="currentColor"
+            class="bi bi-list"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
+            />
+          </svg>
        </button>
        <div
          class="collapse navbar-collapse order-4"
