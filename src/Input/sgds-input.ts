7ffb04e3c0e535468d1c7269af0aac874529d7f4
rav3 <rav3tey@gmail.com>
2022-10-11 10:28:20 +0800

    feat(input): cleanups

--- src/Input/sgds-input.ts
+++ src/Input/sgds-input.ts
@@ -6,10 +6,6 @@ import styles from "./sgds-input.scss";
 import SgdsElement from "../utils/sgds-element";
 import { live } from 'lit/directives/live.js';
 
-export type ButtonVariant = 
-"primary" | "secondary" | "success" | "danger" | "warning" | "info" | "light" | "dark" | "link" |
-"outline-primary" | "outline-secondary" | "outline-success" | "outline-danger" | "outline-warning" | "outline-info" | "outline-light" | "outline-dark"
-
 @customElement("sgds-input")
 export class SgdsInput extends SgdsElement {
   @query('input') input: HTMLInputElement;
@@ -25,21 +21,28 @@ export class SgdsInput extends SgdsElement {
     | 'text'
     | 'time'
     | 'url' = 'text';
-  @property({ type:String, reflect: true }) id = 'defaultID';
-  @property() value = '';
-  @property({ type: String, reflect: true }) inputClasses?;
-  @property({ type: Boolean, reflect: true }) wasValidated = false;
-  @property({ type: String, reflect: true }) feedback = "default feedback";
-  @property({ type: String, reflect: true }) iconName;
   @property({ type: String, reflect: true }) label = "label";
   @property({ type: String, reflect: true}) hintText;
+  @property({ type:String, reflect: true }) id = 'defaultID';
+  @property({ type:String, reflect: true }) name;
+  @property({ type: String, reflect: true }) inputClasses?;
+  @property({ type: String, reflect: true }) iconName;
+
+  @property({ type: String, reflect: true }) value = '';
+  @property({ type: String, reflect: true}) minlength;
+  @property({ type: String, reflect: true}) maxlength;
+
   @property({ type: String, reflect: true }) placeholder = "Placeholder";
+  
+  @property({ type: String, reflect: true }) pattern;
+  @property({ type: String, reflect: true }) feedback = "default feedback";
+  
   @property({ type: Boolean, reflect: true }) autofocus = false;
   @property({ type: Boolean, reflect: true }) disabled = false;
   @property({ type: Boolean, reflect: true }) required = false;
   /** Makes the input readonly. */
   @property({ type: Boolean, reflect: true }) readonly = false;
-  @property({type: String }) pattern;
+  
 
   //TODO get value from input
   handleInput() {
@@ -50,30 +53,25 @@ export class SgdsInput extends SgdsElement {
     this.value = this.input.value;
     this.emit('sgds-change');
   }
-  //TODO set value to input
-  //TODO Validation
-
 
   render() {
     const input = html`
       <input type="text"
-        class="form-control" 
+        class="form-control ${this.inputClasses}" 
         type=${this.type}
         id=${ifDefined(this.id)}
+        name=${ifDefined(this.name)}
         placeholder=${this.placeholder}
-        ?autofocus=${this.autofocus}
         @input=${this.handleInput}
         @change=${this.handleChange}
-        pattern=${this.pattern}
+        pattern=${ifDefined(this.pattern)}
+        ?autofocus=${this.autofocus}
         ?disabled=${this.disabled}
         ?readonly=${this.readonly}
         ?required=${this.required}
-        value=${live(this.value)}
-        aria-label="
-            ${this.disabled ? 'disabled' : undefined}
-            ${this.readonly ? 'readonly' : undefined}
-            ${this.required ? 'required' : undefined}
-          "
+        .value=${live(this.value)}
+        minlength=${ifDefined(this.minlength)}
+        maxlength=${ifDefined(this.maxlength)}
       >
       <div id="${ifDefined(this.id)}Invalid" class="invalid-feedback">${this.feedback}</div>
     `
@@ -84,7 +82,6 @@ export class SgdsInput extends SgdsElement {
           <sl-icon name=${this.iconName}></sl-icon> 
         </span>
         ${input}
-        
       </div>
     `
     // if hintText is defined
@@ -93,7 +90,7 @@ export class SgdsInput extends SgdsElement {
     `
 
     return html`
-      <div class="sgds form-group ${this.inputClasses} ${ifDefined(this.wasValidated) ? 'was-validated' : undefined }">
+      <div class="sgds form-group ${this.inputClasses}">
         <label for=${ifDefined(this.id)} class="form-label">${this.label}</label>
         ${this.hintText ? withHintText : undefined }
         ${this.iconName ? inputWithIcon : input}
