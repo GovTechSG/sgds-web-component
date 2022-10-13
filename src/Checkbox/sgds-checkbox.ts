d19423d89cb3813b909967d4ac6bb611a45e9663
hafizi <61571184+hafiz-ism@users.noreply.github.com>
2022-10-13 16:49:35 +0800

    feat(): updated sb docs, component props and unit test

--- src/Checkbox/sgds-checkbox.ts
+++ src/Checkbox/sgds-checkbox.ts
@@ -4,8 +4,8 @@ import { live } from "lit/directives/live.js";
 import styles from "./sgds-checkbox.scss";
 import { ifDefined } from "lit/directives/if-defined.js";
 import SgdsElement from "../utils/sgds-element";
-import { watch } from "../utils/watch";
 import { classMap } from "lit/directives/class-map.js";
+import genId from "../utils/generateId";
 
 @customElement("sgds-checkbox")
 export class SgdsCheckbox extends SgdsElement {
@@ -15,6 +15,18 @@ export class SgdsCheckbox extends SgdsElement {
 
   /** Name of the HTML form control. Submitted with the form as part of a name/value pair. */
   @property() name: string;
+
+  /** For Id/For pair of the HTML form control. */
+  @property({ type: String, reflect: true }) id = genId("checkbox");
+
+  /** For aria-label when there is no appropriate text label visible */
+  @property({ type: String, reflect: true }) ariaLabel = "checkbox";
+
+  /** Manually style the input as valid */
+  @property({ type: Boolean, reflect: true }) isValid = false;
+
+  /** Manually style the input as invalid */
+  @property({ type: Boolean, reflect: true }) isInvalid = false;
 
   /** Value of the HTML form control. Primarily used to differentiate a list of related checkboxes that have the same name. */
   @property() value: string;
@@ -28,6 +40,9 @@ export class SgdsCheckbox extends SgdsElement {
   /** Disables the checkbox (so the user can't check / uncheck it). */
   @property({ type: Boolean, reflect: true }) disabled = false;
 
+  /** Toggles display feedback */
+  @property({ type: Boolean, reflect: true }) showFeedback = false;
+
   /** Simulates a click on the checkbox. */
   click() {
     this.input.click();
@@ -35,33 +50,52 @@ export class SgdsCheckbox extends SgdsElement {
 
   handleClick() {
     this.checked = !this.checked;
+    if (this.checked == true) this.emit("sgds-checkbox");
   }
 
-  @watch("disabled", { waitUntilFirstUpdate: true })
   handleDisabledChange() {
     // Disabled form controls are always valid, so we need to recheck validity when the state changes
     this.input.disabled = this.disabled;
   }
 
-  @watch("checked", { waitUntilFirstUpdate: true })
   render() {
     return html`
-      <label part="base" class="form-check">
+      <div part="base" class="form-check">
         <input
           class=${classMap({
             "form-check-input": true,
+            "is-invalid": this.isInvalid,
+            "is-valid": this.isValid,
           })}
           type="checkbox"
+          id=${ifDefined(this.id)}
           name=${ifDefined(this.name)}
           value=${ifDefined(this.value)}
           .checked=${live(this.checked)}
-          .disabled=${this.disabled}
-          .required=${this.required}
+          ?disabled=${this.disabled}
+          ?required=${this.required}
+          aria-disabled=${this.disabled ? "true" : "false"}
           aria-checked=${this.checked ? "true" : "false"}
           @click=${this.handleClick}
         />
-        <span part="label" class="form-check-label"><slot></slot></span>
-      </label>
+        <label
+          part="label"
+          for="${ifDefined(this.id)}"
+          aria-label=${ifDefined(this.ariaLabel)}
+          class="form-check-label"
+          ><slot></slot
+        ></label>
+        ${this.showFeedback
+          ? html`<slot
+              name="feedback"
+              part="feedback"
+              class=${classMap({
+                "invalid-feedback": this.isInvalid,
+                "valid-feedback": this.isValid,
+              })}
+            ></slot>`
+          : undefined}
+      </div>
     `;
   }
 }
--- test/sgds-checkbox.test.ts
+++ test/sgds-checkbox.test.ts
@@ -17,10 +17,87 @@ describe("<sgds-checkbox>", () => {
     assert.instanceOf(el, SgdsCheckbox);
   });
 
-  it("should be disabled with the disabled attribute", async () => {
+  it("should be disabled with the disabled attribute & aria-disabled to be true", async () => {
     const el = await fixture(html`<sgds-checkbox disabled></sgds-checkbox>`);
     const checkbox = el.shadowRoot?.querySelector("input");
     expect(checkbox?.disabled).to.be.true;
+    expect(checkbox).to.have.attribute("aria-disabled", "true");
+  });
+
+  it("should be able to pass in value to attributes(id/for pair)", async () => {
+    const el = await fixture(
+      html`<sgds-checkbox id="checkbox-123"></sgds-checkbox>`
+    );
+    const input = el.shadowRoot?.querySelector("input");
+    expect(input).to.have.attribute("id", "checkbox-123");
+    const label = el.shadowRoot?.querySelector("label");
+    expect(label).to.have.attribute("for", "checkbox-123");
+  });
+
+  it("should render id to attributes(id/for pair) by default", async () => {
+    const el = await fixture(html`<sgds-checkbox></sgds-checkbox>`);
+    const input = el.shadowRoot?.querySelector("input");
+    expect(input).to.have.attribute("id");
+    const label = el.shadowRoot?.querySelector("label");
+    expect(label).to.have.attribute("for");
+  });
+
+  it("should render aria-label attribute by default", async () => {
+    const el = await fixture(
+      html`<sgds-checkbox ariaLabel="label"></sgds-checkbox>`
+    );
+    const checkbox = el.shadowRoot?.querySelector("label");
+    expect(checkbox).to.have.attribute("aria-label");
+  });
+
+  it("should be able to pass in value to aria-label attribute", async () => {
+    const el = await fixture(
+      html`<sgds-checkbox ariaLabel="label"></sgds-checkbox>`
+    );
+    const checkbox = el.shadowRoot?.querySelector("label");
+    expect(checkbox).to.have.attribute("aria-label", "label");
+  });
+
+  it("should have class .is-invalid with isInvalid prop", async () => {
+    const el = await fixture(html`<sgds-checkbox isInvalid></sgds-checkbox>`);
+    const checkbox = el.shadowRoot?.querySelector("input");
+    expect(checkbox?.classList.value).to.contain("is-invalid");
+  });
+
+  it("should have class .is-valid with isValid prop", async () => {
+    const el = await fixture(html`<sgds-checkbox isValid></sgds-checkbox>`);
+    const checkbox = el.shadowRoot?.querySelector("input");
+    expect(checkbox?.classList.value).to.contain("is-valid");
+  });
+
+  it("if prop showFeedback is false, feedback element should not be defined", async () => {
+    const el = await fixture(
+      html`<sgds-checkbox isValid
+        ><div slot="feedback">Hello world</div></sgds-checkbox
+      >`
+    );
+    const checkbox = el.shadowRoot?.querySelector("div > slot");
+    expect(checkbox).to.be.null;
+  });
+
+  it("if class input is .is-valid, feedback div should contain .valid-feedback", async () => {
+    const el = await fixture(
+      html`<sgds-checkbox isValid showFeedback
+        ><div slot="feedback">Hello world</div></sgds-checkbox
+      >`
+    );
+    const checkbox = el.shadowRoot?.querySelector("div > slot");
+    expect(checkbox?.classList.value).to.contain("valid-feedback");
+  });
+
+  it("if class input is .is-invalid, feedback div should contain .invalid-feedback", async () => {
+    const el = await fixture(
+      html`<sgds-checkbox isInvalid showFeedback
+        ><div slot="feedback">Hello world</div></sgds-checkbox
+      >`
+    );
+    const checkbox = el.shadowRoot?.querySelector("slot[name='feedback']");
+    expect(checkbox?.classList.value).to.contain("invalid-feedback");
   });
 
   it("should click the inner input", async () => {
