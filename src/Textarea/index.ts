4d41ddfd6e83c2e599c5554637e8ade28cf061d9
rav3 <rav3tey@gmail.com>
2022-10-18 16:34:48 +0800

    feat(textarea): add textarea component

---
+++ src/Textarea/index.ts
@@ -0,0 +1,1 @@
+export { SgdsTextArea } from "./sgds-textarea";
---
+++ src/Textarea/sgds-textarea.ts
@@ -0,0 +1,187 @@
+import { customElement, property,query, state } from "lit/decorators.js";
+import { html } from 'lit/static-html.js';
+import { ifDefined } from 'lit/directives/if-defined.js';
+import {classMap} from 'lit/directives/class-map.js';
+import { live } from 'lit/directives/live.js';
+import styles from "./sgds-textarea.scss";
+import SgdsElement from "../utils/sgds-element";
+import { defaultValue } from "../utils/defaultvalue";
+import { FormSubmitController } from "../utils/form";
+import genId from "../utils/generateId";
+import { watch } from "../utils/watch";
+
+@customElement("sgds-textarea")
+export class SgdsTextArea extends SgdsElement {
+  static styles = styles;
+
+  @query('textarea.form-control') textarea: HTMLTextAreaElement;
+  @state() private hasFocus = false;
+
+  private readonly formSubmitController = new FormSubmitController(this);
+  private resizeObserver: ResizeObserver;
+  
+  
+  @property({ type: String, reflect: true }) label = "label";
+  @property({ type: String, reflect: true}) hintText;
+  @property({ type:String, reflect: true }) id = genId("textarea", null);
+  @property({ type:String, reflect: true }) name;
+  @property({ type: String, reflect: true }) textareaClasses?;
+  @property({ type: String, reflect: true }) value = '';
+  @property({ type: String, reflect: true}) minlength;
+  @property({ type: Number, reflect: true}) maxlength = 200;
+  @property({ type: Boolean, reflect: true}) spellcheck = false;
+   /** The number of rows to display by default. */
+  @property({ type: Number }) rows = 4;
+  @property({ type: String, reflect: true }) placeholder = "Placeholder";
+  @property({ type: String, reflect: true }) invalidFeedback = "default feedback";
+  @property({ type: Boolean, reflect: true }) autofocus = false;
+  @property({ type: Boolean, reflect: true }) disabled = false;
+  @property({ type: Boolean, reflect: true }) required = false;
+  /** Makes the input readonly. */
+  @property({ type: Boolean, reflect: true }) readonly = false;
+  @property({ type: Boolean, reflect: true }) invalid = false;
+  /** Controls how the textarea can be resized. */
+  @property() resize: 'none' | 'vertical' | 'auto' = 'vertical';
+  @property({reflect: true, type: Number}) valueLength = 0;
+
+  /** Gets or sets the default value used to reset this element. The initial value corresponds to the one originally specified in the HTML that created this element. */
+  @defaultValue()
+  defaultValue = '';
+
+  connectedCallback() {
+    super.connectedCallback();
+    this.resizeObserver = new ResizeObserver(() => this.setTextareaHeight());
+
+    this.updateComplete.then(() => {
+      this.setTextareaHeight();
+      this.resizeObserver.observe(this.textarea);
+    });
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    this.resizeObserver.unobserve(this.textarea);
+  }
+
+  firstUpdated() {
+    this.invalid = !this.textarea.checkValidity();
+  }
+
+  /** Sets focus on the textarea. */
+  focus(options?: FocusOptions) {
+    this.textarea.focus(options);
+  }
+
+  /** Checks for validity and shows the browser's validation message if the control is invalid. */
+  reportValidity() {
+    return this.textarea.reportValidity();
+  }
+
+  handleInvalid() {
+    this.invalid = true;
+  }
+
+  handleChange(event: string){
+    this.value = this.textarea.value;
+    this.emit(event);
+  }
+
+  handleFocus() {
+    this.hasFocus = true;
+    this.emit('sgds-focus');
+  }
+
+  handleBlur() {
+    this.hasFocus = false;
+    this.emit('sgds-blur');
+  }
+
+  handleKeyDown(event: KeyboardEvent) {
+    const hasModifier = event.metaKey || event.ctrlKey || event.shiftKey || event.altKey;
+
+    // Pressing enter when focused on an input should submit the form like a native input, but we wait a tick before
+    // submitting to allow users to cancel the keydown event if they need to
+    if (event.key === 'Enter' && !hasModifier) {
+      setTimeout(() => {
+        if (!event.defaultPrevented) {
+          this.formSubmitController.submit();
+        }
+      });
+    }
+  }
+
+  @watch('rows', { waitUntilFirstUpdate: true })
+  handleRowsChange() {
+    this.setTextareaHeight();
+  }
+
+  @watch('value', { waitUntilFirstUpdate: true })
+  handleCharCount() {
+   if(this.value.length > this.maxlength) return this.invalid = true;
+  }
+
+  setTextareaHeight() {
+    if (this.resize === 'auto') {
+      this.textarea.style.height = 'auto';
+      this.textarea.style.height = `${this.textarea.scrollHeight}px`;
+    } else {
+      (this.textarea.style.height as string | undefined) = undefined;
+    }
+  }
+
+  @watch('disabled', { waitUntilFirstUpdate: true })
+  handleDisabledChange() {
+    // Disabled form controls are always valid, so we need to recheck validity when the state changes
+    this.textarea.disabled = this.disabled;
+    this.invalid = !this.textarea.checkValidity();
+  }
+
+  @watch('value', { waitUntilFirstUpdate: true })
+  handleValueChange() {
+    this.invalid = !this.textarea.checkValidity();
+  }
+
+  render() {
+    return html`
+      <div class="sgds form-group 
+      ${classMap(
+          { 
+            [`${this.textareaClasses}`]: this.textareaClasses
+          })}"
+      >
+        <div class="d-flex justify-content-between">
+          <div for=${ifDefined(this.id)} class="form-label">${this.label}</div>
+          <div class="form-text">${this.value.length}/${this.maxlength}</div>
+        </div>
+        <textarea 
+          class="form-control ${classMap(
+          { 
+            'is-invalid' : this.required && this.invalid,
+            'is-valid' : this.required && !this.invalid
+          })}"
+          id=${ifDefined(this.id)}
+          rows=${ifDefined(this.rows)}
+          placeholder=${ifDefined(this.placeholder)}
+          .value=${live(this.value)}
+          aria-invalid=${this.invalid ? 'true' : 'false'}
+          spellcheck=${ifDefined(this.spellcheck)}
+          ?disabled=${this.disabled}
+          ?readonly=${this.readonly}
+          ?required=${this.required}
+          ?autofocus=${this.autofocus}
+          @keyup=${this.handleCharCount}
+          @input=${()=> this.handleChange('sgds-input')}
+          @change=${()=> this.handleChange('sgds-change')}
+          @invalid=${this.handleInvalid}
+          @focus=${this.handleFocus}
+          @blur=${this.handleBlur}
+        >
+        </textarea>
+        <div id="${this.id}-invalid" class="invalid-feedback">${this.invalidFeedback}</div>
+      </div>
+    `;
+
+  }
+}
+
+export default SgdsTextArea;
