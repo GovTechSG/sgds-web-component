1a5affc63626e7c65c65ccc74fa8ce45b9c7e1af
hafizi <61571184+hafiz-ism@users.noreply.github.com>
2022-10-21 11:37:43 +0800

    fix(checkbox): added form controller to checkbox comp, refix some props

--- src/Checkbox/sgds-checkbox.ts
+++ src/Checkbox/sgds-checkbox.ts
@@ -1,11 +1,14 @@
 import { LitElement, html } from "lit";
-import { customElement, property, query } from "lit/decorators.js";
+import { customElement, property, query, state } from "lit/decorators.js";
 import { live } from "lit/directives/live.js";
 import styles from "./sgds-checkbox.scss";
 import { ifDefined } from "lit/directives/if-defined.js";
 import SgdsElement from "../utils/sgds-element";
 import { classMap } from "lit/directives/class-map.js";
 import genId from "../utils/generateId";
+import { defaultValue } from "../utils/defaultvalue";
+import { FormSubmitController } from "../utils/form";
+import { watch } from "../utils/watch";
 
 @customElement("sgds-checkbox")
 export class SgdsCheckbox extends SgdsElement {
@@ -13,20 +16,28 @@ export class SgdsCheckbox extends SgdsElement {
 
   @query('input[type="checkbox"]') input: HTMLInputElement;
 
+  private readonly formSubmitController = new FormSubmitController(this, {
+    value: (control: SgdsCheckbox) =>
+      control.checked ? control.value : undefined,
+    defaultValue: (control: SgdsCheckbox) => control.defaultChecked,
+    setValue: (control: SgdsCheckbox, checked: boolean) =>
+      (control.checked = checked),
+  });
+
   /** Name of the HTML form control. Submitted with the form as part of a name/value pair. */
   @property() name: string;
 
   /** For Id/For pair of the HTML form control. */
-  @property({ type: String, reflect: true }) id = genId("checkbox");
+  @property({ type: String, reflect: true }) inputId = genId("checkbox");
 
   /** For aria-label when there is no appropriate text label visible */
   @property({ type: String, reflect: true }) ariaLabel = "checkbox";
 
   /** Manually style the input as valid */
-  @property({ type: Boolean, reflect: true }) isValid = false;
+  // @property({ type: Boolean, reflect: true }) valid = false;
 
   /** Manually style the input as invalid */
-  @property({ type: Boolean, reflect: true }) isInvalid = false;
+  @property({ type: Boolean, reflect: true }) invalid = false;
 
   /** Value of the HTML form control. Primarily used to differentiate a list of related checkboxes that have the same name. */
   @property() value: string;
@@ -40,22 +51,59 @@ export class SgdsCheckbox extends SgdsElement {
   /** Disables the checkbox (so the user can't check / uncheck it). */
   @property({ type: Boolean, reflect: true }) disabled = false;
 
-  /** Toggles display feedback */
-  @property({ type: Boolean, reflect: true }) showFeedback = false;
+  // /** Toggles display feedback */
+  // @property({ type: Boolean, reflect: true }) showFeedback = false;
+
+  /** Gets or sets the default value used to reset this element. The initial value corresponds to the one originally specified in the HTML that created this element. */
+  @defaultValue("checked")
+  defaultChecked = false;
+
+  firstUpdated() {
+    this.invalid = !this.input.checkValidity();
+  }
 
   /** Simulates a click on the checkbox. */
   click() {
     this.input.click();
   }
 
-  handleClick() {
-    this.checked = !this.checked;
-    if (this.checked == true) this.emit("sgds-change");
+  /** Checks for validity and shows the browser's validation message if the control is invalid. */
+  reportValidity() {
+    return this.input.reportValidity();
   }
 
+  // handleClick() {
+  //   this.checked = !this.checked;
+  //   this.value = this.input.value;
+  //   this.emit("sgds-click");
+  // }
+
+  handleChange(event: string) {
+    this.checked = !this.checked;
+    this.value = this.input.value;
+    this.emit(event);
+  }
+
+  // handleKeyDown(event: KeyboardEvent) {
+  //   const hasModifier =
+  //     event.metaKey || event.ctrlKey || event.shiftKey || event.altKey;
+  //   if (event.key === "Enter" && !hasModifier) {
+  //     this.checked = !this.checked;
+  //   } else if(event.key === "Enter" && !hasModifier && this.input.required && this.invalid) {
+  //     this.checked = !this.checked;
+  //   }
+  // }
+
+  @watch("disabled", { waitUntilFirstUpdate: true })
   handleDisabledChange() {
     // Disabled form controls are always valid, so we need to recheck validity when the state changes
     this.input.disabled = this.disabled;
+    this.invalid = !this.input.checkValidity();
+  }
+
+  @watch("checked", { waitUntilFirstUpdate: true })
+  handleStateChange() {
+    this.invalid = !this.input.checkValidity();
   }
 
   render() {
@@ -65,11 +113,12 @@ export class SgdsCheckbox extends SgdsElement {
           part="control"
           class=${classMap({
             "form-check-input": true,
-            "is-invalid": this.isInvalid,
-            "is-valid": this.isValid,
+            "is-invalid": this.required && this.invalid,
+            // "is-valid": this.required && this.valid,
           })}
           type="checkbox"
-          id=${ifDefined(this.id)}
+          id=${ifDefined(this.inputId)}
+          aria-invalid=${this.invalid ? "true" : "false"}
           name=${ifDefined(this.name)}
           value=${ifDefined(this.value)}
           .checked=${live(this.checked)}
@@ -77,22 +126,22 @@ export class SgdsCheckbox extends SgdsElement {
           ?required=${this.required}
           aria-disabled=${this.disabled ? "true" : "false"}
           aria-checked=${this.checked ? "true" : "false"}
-          @click=${this.handleClick}
+          @change=${() => this.handleChange("sgds-change")}
         />
         <label
           part="label"
-          for="${ifDefined(this.id)}"
+          for="${ifDefined(this.inputId)}"
           aria-label=${ifDefined(this.ariaLabel)}
           class="form-check-label"
           ><slot></slot
         ></label>
-        ${this.showFeedback
+        ${this.invalid
           ? html`<slot
               name="feedback"
               part="feedback"
               class=${classMap({
-                "invalid-feedback": this.isInvalid,
-                "valid-feedback": this.isValid,
+                "invalid-feedback": true,
+                // "valid-feedback": this.valid,
               })}
             ></slot>`
           : undefined}
