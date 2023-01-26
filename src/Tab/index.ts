dddec48f60019c55797722677c7e67f78b4d607f
rav3 <rav3tey@gmail.com>
2023-01-26 16:38:12 +0800

    feat(tab): add tab component

---
+++ src/Tab/index.ts
@@ -0,0 +1,3 @@
+export { SgdsTab } from './sgds-tab';
+export { SgdsTabPanel } from './sgds-tabpanel';
+export { SgdsTabGroup } from './sgds-tabgroup';
---
+++ src/utils/offset.ts
@@ -0,0 +1,6 @@
+export function getOffset(element: HTMLElement, parent: HTMLElement) {
+    return {
+      top: Math.round(element.getBoundingClientRect().top - parent.getBoundingClientRect().top),
+      left: Math.round(element.getBoundingClientRect().left - parent.getBoundingClientRect().left)
+    };
+  }
