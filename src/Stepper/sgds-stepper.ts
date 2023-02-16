1c61dcba70d6f57998d07839388beb7732be8b1e
hafizi <61571184+hafiz-ism@users.noreply.github.com>
2023-02-16 10:10:00 +0800

    feat(stepper): added stepper component

--- src/Stepper/sgds-stepper.ts
+++ src/Stepper/sgds-stepper.ts
@@ -1,69 +1,8 @@
 import { css, html } from "lit";
 import SgdsElement from "../utils/sgds-element";
-import { customElement, property } from "lit/decorators.js";
+import { customElement, property, query } from "lit/decorators.js";
 import styles from "./sgds-stepper.scss";
 import { classMap } from "lit/directives/class-map.js";
-
-// export interface StepMetaData {
-//   component: any;
-//   title: string;
-//   stepHeader: string;
-// }
-
-// const getClass = (stepMetadata: StepMetaData) => {
-//   if (stepMetadata.step < state.currentStep) {
-//     return "is-completed is-clickable";
-//   }
-//   if (stepMetadata.step === state.currentStep) {
-//     return "is-active";
-//   }
-//   return "";
-// };
-
-// export class WrappedStepsMetadata {
-//   private wrappedStepMetadataArr :WrappedStepMetadata[];
-//   constructor(wrappedStepMetadataArr: WrappedStepMetadata[]) {
-//     this.wrappedStepMetadataArr = wrappedStepMetadataArr;
-//   }
-
-//   get length() {
-//     return this.wrappedStepMetadataArr.length;
-//   }
-
-//   findByStep(step: number) {
-//     return this.wrappedStepMetadataArr.find(
-//       (wrappedStepMetadata) => wrappedStepMetadata.step === step
-//     );
-//   }
-
-//   isFirstStep(step: number) {
-//     return step === 1
-//   }
-
-//   isLastStep(step: number) {
-//     return step === this.length;
-//   }
-
-//   isWithinStepRange(step: number) {
-//     return step >= 1 && step <= this.length + 1;
-//   }
-
-//   get data() {
-//     return this.wrappedStepMetadataArr;
-//   }
-// }
-
-// next() {
-//   if (this.activeStep < this.steps.length - 1) {
-//     this.activeStep++;
-//   }
-// }
-
-// back() {
-//   if (this.activeStep > 0) {
-//     this.activeStep--;
-//   }
-// }
 
 @customElement("sgds-stepper")
 export class SgdsStepper extends SgdsElement {
@@ -80,22 +19,39 @@ export class SgdsStepper extends SgdsElement {
   ];
 
   @property({ type: Number })
-  activeStep = 2;
-
-  @property({ type: Object }) stepper: SgdsStepper;
-
-  @property({ type: Array })
-  _isSlotVisible = new Array(this.steps.length).fill(false);
+  activeStep: number;
 
   connectedCallback() {
     super.connectedCallback();
-    this.stepper = this;
+  }
+
+  public incrementStep() {
+    if (this.activeStep < this.steps.length - 1) {
+      this.activeStep++;
+    }
+  }
+
+  public decrementStep() {
+    if (this.activeStep > 0) {
+      this.activeStep--;
+    }
+  }
+
+  public lastStep() {
+    if (this.activeStep !== this.steps.length - 1) {
+      this.activeStep = this.steps.length - 1;
+    }
+  }
+
+  public firstStep() {
+    if (this.activeStep > 0) {
+      this.activeStep = 0;
+    }
   }
 
   _onStepperItemClick(index: number) {
     if (this.activeStep > index) {
       this.activeStep = index;
-      this._isSlotVisible[index] = !this._isSlotVisible[index];
     }
   }
 
@@ -117,31 +73,16 @@ export class SgdsStepper extends SgdsElement {
                 <p><b>${step.stepHeader}</b></p>
               </div>
             </div>
-            <br /><br />
-            ${this.activeStep === index
-              ? html`<p slot=${index}>This is the external content for step-${index}</p>`
-              : html``}
           `;
         })}
       </div>
+      <slot
+        name="step-${this.activeStep + 1}"
+        ?hidden="${this.activeStep === -1}"
+      ></slot>
     `;
   }
 }
 
 export default SgdsStepper;
 
-// <!-- ${this.steps.map(
-//   (step, index) => html`
-//     <div
-//       class="stepper-item ${classMap({
-//         "is-active": this.activeStep == index,
-//         "is-completed is-clickable": this.activeStep > index,
-//       })}"
-//     >
-//       <div class="stepper-marker">${step.marker}</div>
-//       <div class="stepper-detail">
-//         <p><b>${step.title}</b></p>
-//       </div>
-//     </div>
-//   `
-// )} -->
