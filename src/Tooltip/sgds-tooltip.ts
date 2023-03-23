bd9f40f72ab9b75de60ad96e42d15f1d7abcd7c9
clukhei <kheiboardwarrior@gmail.com>
2023-03-23 20:31:01 +0800

    feat(tooltip): added closable tooltip

--- src/Tooltip/sgds-tooltip.ts
+++ src/Tooltip/sgds-tooltip.ts
@@ -1,20 +1,23 @@
 import { html } from "lit";
-import { customElement, property, query } from "lit/decorators.js";
+import { customElement, property, query, queryAsync } from "lit/decorators.js";
 import SgdsElement from "../utils/sgds-element";
 import { ref, createRef, Ref } from "lit/directives/ref.js";
 import { Tooltip } from "bootstrap";
+import styles from "./sgds-tooltip.scss";
 
 @customElement("sgds-tooltip")
 export class SgdsTooltip extends SgdsElement {
+  static styles = styles;
+
   myTooltip: Ref<HTMLElement> = createRef();
   bsTooltip: Tooltip = null;
 
-  @query('.tooltip-inner') tooltipInner: HTMLDivElement
+  @query(".tooltip-inner") tooltipInner: HTMLDivElement;
 
   @property({ type: String })
   content = "hello";
   @property({ type: Array })
-  offset: [number, number] = [0,0];
+  offset: [number, number] = [0, 0];
 
   @property({ type: String })
   placement: "top" | "bottom" | "left" | "right" = "top";
@@ -22,14 +25,42 @@ export class SgdsTooltip extends SgdsElement {
   @property({ type: String })
   trigger: "click" | "hover" | "focus" | "hover focus" = "hover focus";
 
+  private closableContainer: HTMLElement 
+
   firstUpdated() {
+    // refer to Bootstrap's Tooltip options
+    // Feature: Add close button when prop trigger === 'click'
+    //useless to modify the "template" as BsTooltip "title" option will override anything that is within .tooltip-inner
+    //HTML method insertAdjacentText() is use to add the text content before close button but it requires a parent
+    // Only way is to modify BsTooltip's title with "sanitize: false" to add the close button
+    // When trigger is a "click", tooltipContainer will be created and modify BsTooltip's "title", adding a close button
+    if (this.trigger === "click") {
+       this.closableContainer = document.createElement("div");
+      this.closableContainer.classList.add("d-flex");
+      this.closableContainer.classList.add("gap-5");
+      let closeBtn: HTMLButtonElement = document.createElement("button");
+      closeBtn.classList.add("btn-close");
+      closeBtn.classList.add("btn-close-white");
+      closeBtn.setAttribute("aria-label", "Close");
+      this.closableContainer.appendChild(closeBtn);
+      this.closableContainer.insertAdjacentText("afterbegin", this.content);
+
+      this.shadowRoot
+      .querySelector(".btn-close")
+      ?.addEventListener("click", () => this.closeTooltip());
+    }
+
     this.bsTooltip = new Tooltip(this.myTooltip.value, {
       trigger: this.trigger,
-      title: this.content,
+      title: this.trigger === "click" ? this.closableContainer : this.content,
       placement: this.placement,
-      offset:this.offset,
-      html: true
+      offset: this.offset,
+      html: true,
+      sanitize: false, // to allow button element,
+      container: this.shadowRoot.querySelector("span"), // tooltip to appear inside the shadow root of sgds-tooltip instead of anywhere in the DOM, so that scoped styles can apply
     });
+
+    
     this.myTooltip.value.addEventListener("show.bs.tooltip", () => {
       this.emit("sgds-show");
     });
@@ -42,27 +73,12 @@ export class SgdsTooltip extends SgdsElement {
     this.myTooltip.value.addEventListener("hidden.bs.tooltip", () => {
       this.emit("sgds-hidden");
     });
-
-   
-
   }
-  closeTooltip(){
-    this.bsTooltip.hide()
+  closeTooltip() {
+    this.bsTooltip.hide();
   }
-  updated() {
-    if (this.trigger === 'click') {
-      console.log('hello')
-      let closeBtn : HTMLButtonElement  = document.createElement('button')
-      closeBtn.classList.add('btn-close')
-      closeBtn.classList.add('btn-close-white')
-      this.bsTooltip.setContent({ '.tooltip-inner': closeBtn })
 
-      console.log(this.tooltipInner.shadowRoot.innerHTML)
-      this.tooltipInner.insertAdjacentElement("beforeend", closeBtn)
-    }
-  }
   render() {
-
     return html`
       <span ${ref(this.myTooltip)}>
         <slot></slot>
