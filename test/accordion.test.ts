c85852c2211387eb1fa5b2a6d8a8c33889c5376e
rav3 <rav3tey@gmail.com>
2023-04-10 12:49:05 +0800

    test(accordion): update accordion tag

--- test/accordion.test.ts
+++ test/accordion.test.ts
@@ -1,17 +1,18 @@
 import { SgdsAccordionItem } from '../src/Accordion/sgds-accordion-item';
 import '../src/Accordion/sgds-accordion-item';
-import {fixture, assert, expect, aTimeout, waitUntil} from '@open-wc/testing';
+import {fixture, expect, waitUntil} from '@open-wc/testing';
 import {html} from 'lit';
 import sinon  from 'sinon';
 
-describe('<sgds-accordion>', () => {
+
+describe('<sgds-accordion-item>', () => {
     it('should be visible with the open attribute', async () => {
       const el = await fixture<SgdsAccordionItem>(html`
-        <sgds-accordion open>
+        <sgds-accordion-item open>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
           magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
           consequat.
-        </sgds-accordion>
+        </sgds-accordion-item>
       `);
       const body = el.shadowRoot!.querySelector<HTMLElement>('.accordion-body')!;
   
@@ -20,11 +21,11 @@ describe('<sgds-accordion>', () => {
   
     it('should not be visible without the open attribute', async () => {
       const el = await fixture<SgdsAccordionItem>(html`
-        <sgds-accordion>
+        <sgds-accordion-item>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
           magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
           consequat.
-        </sgds-accordion>
+        </sgds-accordion-item>
       `);
       const body = el.shadowRoot!.querySelector<HTMLElement>('.accordion-body')!;
   
@@ -33,11 +34,11 @@ describe('<sgds-accordion>', () => {
   
     it('should emit sgds-show and sgds-after-show when calling show()', async () => {
       const el = await fixture<SgdsAccordionItem>(html`
-        <sgds-accordion>
+        <sgds-accordion-item>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
           magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
           consequat.
-        </sgds-accordion>
+        </sgds-accordion-item>
       `);
       const body = el.shadowRoot!.querySelector<HTMLElement>('.accordion-body')!;
       const showHandler = sinon.spy();
@@ -57,11 +58,11 @@ describe('<sgds-accordion>', () => {
   
     it('should emit sgds-hide and sgds-after-hide when calling hide()', async () => {
       const el = await fixture<SgdsAccordionItem>(html`
-        <sgds-accordion open>
+        <sgds-accordion-item open>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
           magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
           consequat.
-        </sgds-accordion>
+        </sgds-accordion-item>
       `);
       const body = el.shadowRoot!.querySelector<HTMLElement>('.accordion-body')!;
       const hideHandler = sinon.spy();
@@ -81,11 +82,11 @@ describe('<sgds-accordion>', () => {
   
     it('should emit sgds-show and sgds-after-show when setting open = true', async () => {
       const el = await fixture<SgdsAccordionItem>(html`
-        <sgds-accordion>
+        <sgds-accordion-item>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
           magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
           consequat.
-        </sgds-accordion>
+        </sgds-accordion-item>
       `);
       const body = el.shadowRoot!.querySelector<HTMLElement>('.accordion-body')!;
       const showHandler = sinon.spy();
@@ -105,11 +106,11 @@ describe('<sgds-accordion>', () => {
   
     it('should emit sgds-hide and sgds-after-hide when setting open = false', async () => {
       const el = await fixture<SgdsAccordionItem>(html`
-        <sgds-accordion open>
+        <sgds-accordion-item open>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
           magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
           consequat.
-        </sgds-accordion>
+        </sgds-accordion-item>
       `);
       const body = el.shadowRoot!.querySelector<HTMLElement>('.accordion-body')!;
       const hideHandler = sinon.spy();
@@ -129,11 +130,11 @@ describe('<sgds-accordion>', () => {
   
     it('should not open when preventing sgds-show', async () => {
       const el = await fixture<SgdsAccordionItem>(html`
-        <sgds-accordion>
+        <sgds-accordion-item>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
           magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
           consequat.
-        </sgds-accordion>
+        </sgds-accordion-item>
       `);
       const showHandler = sinon.spy((event: Event) => event.preventDefault());
   
@@ -148,11 +149,11 @@ describe('<sgds-accordion>', () => {
   
     it('should not close when preventing sgds-hide', async () => {
       const el = await fixture<SgdsAccordionItem>(html`
-        <sgds-accordion open>
+        <sgds-accordion-item open>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
           magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
           consequat.
-        </sgds-accordion>
+        </sgds-accordion-item>
       `);
       const hideHandler = sinon.spy((event: Event) => event.preventDefault());
   
