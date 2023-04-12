54048c757dbea393871c6d6b3d1d1217f4ba5758
hafizi <61571184+hafiz-ism@users.noreply.github.com>
2023-04-12 11:32:35 +0800

    fix(fileupload): replace sl-icon to SVG icon

--- src/FileUpload/sgds-fileupload.ts
+++ src/FileUpload/sgds-fileupload.ts
@@ -2,6 +2,7 @@ import { html } from "lit";
 import { customElement, property } from "lit/decorators.js";
 import styles from "./sgds-fileupload.scss";
 import SgdsElement from "../utils/sgds-element";
+import { unsafeSVG } from "lit/directives/unsafe-svg.js";
 import genId from "../utils/generateId";
 import { createRef, ref } from "lit/directives/ref.js";
 
@@ -41,10 +42,10 @@ export class SgdsFileUpload extends SgdsElement {
   @property({ reflect: true }) size: "sm" | "lg";
 
   @property({ type: String })
-  checkedIcon = "check";
+  checkedIcon? = "";
 
   @property({ type: String })
-  cancelIcon = "x-circle";
+  cancelIcon? = "";
 
   @property({ type: FileList })
   private fileList: FileList | undefined;
@@ -54,28 +55,29 @@ export class SgdsFileUpload extends SgdsElement {
 
   constructor() {
     super();
-    this.addEventListener('sgds-change', () => {
+    this.addEventListener("sgds-change", () => {
       this.selectedFiles = Array.from(this.fileList || []);
-      this.dispatchEvent(new CustomEvent('selected-files-changed', {
-        detail: { selectedFiles: this.selectedFiles },
-        bubbles: true,
-        composed: true
-      }));
+      this.dispatchEvent(
+        new CustomEvent("selected-files-changed", {
+          detail: { selectedFiles: this.selectedFiles },
+          bubbles: true,
+          composed: true,
+        })
+      );
     });
   }
 
   setFileList(fileList) {
     this.fileList = fileList[0];
-    this.dispatchEvent(new CustomEvent('sgds-change', { bubbles: true, composed: true }));
+    this.dispatchEvent(
+      new CustomEvent("sgds-change", { bubbles: true, composed: true })
+    );
   }
-
 
   static styles = styles;
 
   // Create a ref to the input element
   private inputRef = createRef<HTMLInputElement>();
-
-
 
   private handleClick(event: Event) {
     event.preventDefault();
@@ -95,7 +97,6 @@ export class SgdsFileUpload extends SgdsElement {
     for (let i = 0; i < fileList.length; i++) {
       dt.items.add(fileList[i]);
     }
-
 
     this.setFileList(dt.files);
     this.selectedFiles = Array.from(dt.files);
@@ -124,27 +125,57 @@ export class SgdsFileUpload extends SgdsElement {
   }
 
   render() {
-    const checkedIcon = html`<sl-icon
-      name=${this.checkedIcon}
-      class="align-middle me-2"
-    ></sl-icon>`;
-    const cancelIcon = (index: number) => html`
-      <sl-icon
-        name=${this.cancelIcon}
-        class="align-middle ms-2"
-        @click=${() => this.removeFileHandler(index)}
-      ></sl-icon>
-    `;
+    const getCheckedIcon = (checkedIcon: string) => {
+      if (checkedIcon) {
+        return html`${unsafeSVG(checkedIcon)}`;
+      }
+      return html`<svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="green"
+        class="bi bi-check align-middle me-2"
+        viewBox="0 0 16 16"
+      >
+        <path
+          d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"
+        />
+      </svg>`;
+    };
+
+    const getCancelIcon = (cancelIcon: string) => {
+      if (cancelIcon) {
+        return html`${unsafeSVG(cancelIcon)}`;
+      }
+      return html`<svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="red"
+        class="bi bi-x-circle align-middle ms-2"
+        viewBox="0 0 16 16"
+      >
+        <path
+          d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"
+        />
+        <path
+          d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"
+        />
+      </svg>`;
+    };
 
     const listItems = this.selectedFiles.map(
       (file, index) => html`
         <li key=${index} class="fileupload-list-item">
-          ${checkedIcon}
+          ${getCheckedIcon(this.checkedIcon)}
           <span class="filename">${file.name}</span>
-          ${cancelIcon(index)}
+          <span @click=${() => this.removeFileHandler(index)}
+            >${getCancelIcon(this.cancelIcon)}</span
+          >
         </li>
       `
     );
+
     return html`
       <form>
         <div>
