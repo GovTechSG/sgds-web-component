4ce457ac408395f8193130c696ef19fe82e2e528
hafizi <61571184+hafiz-ism@users.noreply.github.com>
2023-04-17 09:09:38 +0800

    test(fileupload): updated event name to sgds-files-selected

--- test/fileupload.test.ts
+++ test/fileupload.test.ts
@@ -97,7 +97,7 @@ describe("sgds-fileupload", () => {
     );
     const input = el.shadowRoot!.querySelector<HTMLInputElement>("input");
     if (input) {
-      const promise = oneEvent(el, "sgds-change"); // add event listener to the component
+      const promise = oneEvent(el, "sgds-files-selected"); // add event listener to the component
       input.files = dt.files;
       const changeEvent = new Event("change");
       input.dispatchEvent(changeEvent);
@@ -132,7 +132,7 @@ describe("sgds-fileupload", () => {
     );
     const input = el.shadowRoot!.querySelector<HTMLInputElement>("input");
     if (input) {
-      const promise = oneEvent(el, "sgds-change"); // add event listener to the component
+      const promise = oneEvent(el, "sgds-files-selected"); // add event listener to the component
       input.files = dt.files;
       const changeEvent = new Event("change");
       input.dispatchEvent(changeEvent);
@@ -164,11 +164,11 @@ describe("sgds-fileupload", () => {
 
     const el = await fixture<SgdsFileUpload>(
       html`<sgds-fileupload
-        checkedIcon='<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2-circle align-middle ms-2" viewBox="0 0 16 16">
+        checkedIcon='<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2-circle" viewBox="0 0 16 16">
       <path d="M2.5 8a5.5 5.5 0 0 1 8.25-4.764.5.5 0 0 0 .5-.866A6.5 6.5 0 1 0 14.5 8a.5.5 0 0 0-1 0 5.5 5.5 0 1 1-11 0z"/>
       <path d="M15.354 3.354a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l7-7z"/>
     </svg>'
-        cancelIcon='<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-octagon align-middle me-2" viewBox="0 0 16 16">
+        cancelIcon='<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-octagon" viewBox="0 0 16 16">
       <path d="M4.54.146A.5.5 0 0 1 4.893 0h6.214a.5.5 0 0 1 .353.146l4.394 4.394a.5.5 0 0 1 .146.353v6.214a.5.5 0 0 1-.146.353l-4.394 4.394a.5.5 0 0 1-.353.146H4.893a.5.5 0 0 1-.353-.146L.146 11.46A.5.5 0 0 1 0 11.107V4.893a.5.5 0 0 1 .146-.353L4.54.146zM5.1 1 1 5.1v5.8L5.1 15h5.8l4.1-4.1V5.1L10.9 1H5.1z"/>
       <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
     </svg>'
@@ -177,7 +177,7 @@ describe("sgds-fileupload", () => {
     );
     const input = el.shadowRoot!.querySelector<HTMLInputElement>("input");
     if (input) {
-      const promise = oneEvent(el, "sgds-change"); // add event listener to the component
+      const promise = oneEvent(el, "sgds-files-selected"); // add event listener to the component
       input.files = dt.files;
       const changeEvent = new Event("change");
       input.dispatchEvent(changeEvent);
