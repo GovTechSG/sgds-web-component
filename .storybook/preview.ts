495f3690e72f2a1e332002a7728875efe1ea42bd
clukhei <kheiboardwarrior@gmail.com>
2023-04-22 00:20:09 +0800

    chore(*): restore the customElements in preview storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,3 +1,30 @@
+import { setCustomElements } from "@storybook/web-components";
+import customElements from "../custom-elements.json";
+
+// import { setCustomElementsManifest } from '@storybook/web-components';
+
+// export const setCustomElementsManifestWithOptions = (
+//   customElements: any,
+//   options: { privateFields?: boolean },
+// ): void => {
+//   let { privateFields = true } = options;
+//   if (!privateFields) {
+//     customElements?.modules?.forEach((module: { declarations: any[] }) => {
+//       module?.declarations?.forEach(declaration => {
+//         Object.keys(declaration).forEach(key => {
+//           if (Array.isArray(declaration[key])) {
+//             declaration[key] = declaration[key].filter(
+//               (member: { privacy: string | string[] }) =>
+//                 !member.privacy?.includes('private'),
+//             );
+//           }
+//         });
+//       });
+//     });
+//   }
+//   return setCustomElementsManifest(customElements);
+// };
+setCustomElements(customElements, { privateFields: false });
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
