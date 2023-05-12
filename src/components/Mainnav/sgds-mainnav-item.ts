87904dfbbb3b96cedb251489094276cc3b951656
rav3 <rav3tey@gmail.com>
2023-05-12 10:08:58 +0800

    docs(mainnav-item): add more custom css variables

--- src/components/Mainnav/sgds-mainnav-item.ts
+++ src/components/Mainnav/sgds-mainnav-item.ts
@@ -5,7 +5,9 @@ import LinkElement from "../../base/link-element";
 /**
  * @slot default - slot for SgdsMainnavItem element.
  *
- * @cssproperty ---mainnav-item-theme-color - Hover and active color for mainnav items.
+ * @cssproperty --mainnav-item-theme-color - Hover and active color for mainnav items.
+ * @cssproperty --mainnav-item-color - Text color of nav item.
+ * @cssproperty --mainnav-item-borderBottom-width - border bottom width for hover and active state for nav item
  */
 @customElement("sgds-mainnav-item")
 export class SgdsMainnavItem extends LinkElement {
