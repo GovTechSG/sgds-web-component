a9618878249691a8496b6fcc42ee3a8dbe573ab9
rav3 <rav3tey@gmail.com>
2023-05-30 21:23:27 +0800

    fix(fileuploader): add multiple and accept args

--- stories/templates/FileUpload/basic.js
+++ stories/templates/FileUpload/basic.js
@@ -7,6 +7,8 @@ export const Template = args => html`
     .disabled=${args.disabled}
     .checkedIcon=${args.checkedIcon}
     .cancelIcon=${args.cancelIcon}
+    .accept=${args.accept}
+    ?multiple=${args.multiple}
     ><svg
       xmlns="http://www.w3.org/2000/svg"
       width="16"
