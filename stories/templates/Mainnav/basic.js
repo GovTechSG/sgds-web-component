9fcc77b13f5abf03ba56a0fe53a1d6c154bbc91e
clukhei <kheiboardwarrior@gmail.com>
2023-06-12 14:51:48 +0800

    style(*): lint fix

--- stories/templates/Mainnav/basic.js
+++ stories/templates/Mainnav/basic.js
@@ -4,9 +4,7 @@ export const Template = ({ expand, brandHref, active, href, disabled, menuIsOpen
   return html`
     <sgds-mainnav .expand=${expand} .brandHref=${brandHref}>
       <img width="130" src="https://www.designsystem.tech.gov.sg/assets/img/logo-sgds.svg" slot="brand" />
-      <sgds-mainnav-item .active=${active} .href=${href} ?disabled=${disabled}
-        >ArgsTable Controlled
-      </sgds-mainnav-item>
+      <sgds-mainnav-item .active=${active} .href=${href} ?disabled=${disabled}>ArgsTable Controlled </sgds-mainnav-item>
       <sgds-mainnav-dropdown togglerText=${togglerText} ?menuIsOpen=${menuIsOpen} close=${close}>
         <sgds-dropdown-item href="https://google.com">Item 1</sgds-dropdown-item>
         <sgds-dropdown-item href="#">Item 2</sgds-dropdown-item>
