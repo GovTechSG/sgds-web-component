e1c0b57daf73e76df7f9454e5da0140efd84c94d
clukhei <kheiboardwarrior@gmail.com>
2023-06-12 14:50:07 +0800

    docs(mainnav): update controls

--- stories/templates/Mainnav/basic.js
+++ stories/templates/Mainnav/basic.js
@@ -1,13 +1,13 @@
 import { html } from "lit-html";
 
-export const Template = ({ expand, brandHref, active, href, disabledMNI }) => {
+export const Template = ({ expand, brandHref, active, href, disabled, menuIsOpen, togglerText, close }) => {
   return html`
     <sgds-mainnav .expand=${expand} .brandHref=${brandHref}>
       <img width="130" src="https://www.designsystem.tech.gov.sg/assets/img/logo-sgds.svg" slot="brand" />
-      <sgds-mainnav-item .active=${active} .href=${href} .disabled=${disabledMNI}
+      <sgds-mainnav-item .active=${active} .href=${href} ?disabled=${disabled}
         >ArgsTable Controlled
       </sgds-mainnav-item>
-      <sgds-mainnav-dropdown togglerText="Dropdown">
+      <sgds-mainnav-dropdown togglerText=${togglerText} ?menuIsOpen=${menuIsOpen} close=${close}>
         <sgds-dropdown-item href="https://google.com">Item 1</sgds-dropdown-item>
         <sgds-dropdown-item href="#">Item 2</sgds-dropdown-item>
         <sgds-dropdown-item href="#">Item 3</sgds-dropdown-item>
@@ -25,7 +25,8 @@ export const Template = ({ expand, brandHref, active, href, disabledMNI }) => {
   `;
 };
 
-export const args = {};
-// export const storyProps = {
-//   height: "500px",
-// }
+export const args = {
+  togglerText: "Dropdown"
+};
+
+export const parameters = {};
