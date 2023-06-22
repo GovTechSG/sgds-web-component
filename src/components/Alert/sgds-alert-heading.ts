647cc08ac881bfd244a3e89e5c88f94114f535a2
clukhei <kheiboardwarrior@gmail.com>
2023-06-22 15:36:44 +0800

    test(alert): add test for alert heading and link

--- src/components/Alert/sgds-alert-heading.ts
+++ src/components/Alert/sgds-alert-heading.ts
@@ -1,24 +1,18 @@
 import { customElement, property } from "lit/decorators.js";
-import { classMap } from "lit/directives/class-map.js";
 import { html } from "lit/static-html.js";
 import SgdsElement from "../../base/sgds-element";
-import styles from "./sgds-alert.scss";
 
 @customElement("sgds-alert-heading")
 export class SgdsAlertHeading extends SgdsElement {
   static styles = [SgdsElement.styles];
 
-  /** The alert's theme variant. */
-
   @property({ type: String, reflect: true }) headerTag: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" = "h4";
 
   render() {
     return html`
-      <${this.headerTag}
-      class=${this.headerTag}
-      >
-      <slot>
-      </${this.headerTag}>
+      <span class=${this.headerTag}>
+        <slot></slot>
+      </span>
     `;
   }
 }
