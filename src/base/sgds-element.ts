6d53394879a735fd3ae5b376656d76a5f737bf5f
rav3 <rav3tey@gmail.com>
2023-07-03 10:34:49 +0800

    docs(styling): documentation for generated custom variable colors

--- src/base/sgds-element.ts
+++ src/base/sgds-element.ts
@@ -1,5 +1,13 @@
 import { LitElement } from "lit";
 import styles from "./sgds-element.scss";
+
+/**
+ * @cssprop --sgds-{stateColor} - State colors in hexadecimal value
+ * @cssprop --sgds-{stateColor}-rgb - State colors in rgb value
+ * @cssprop --sgds-{stateColor}-{weights} - State colors with different weightage in hexadecimal value
+ * @cssprop --sgds-gray-{weights} - State colors with different weightage in hexadecimal value
+ */
+
 export default class SgdsElement extends LitElement {
   static styles = styles;
   /** Emits a custom event with more convenient defaults. */
