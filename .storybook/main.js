86ed587e5130eb9896f355f77312c36623e249e7
clukhei <kheiboardwarrior@gmail.com>
2023-07-04 15:03:42 +0800

    chore(*): prep for release

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,35 +1,44 @@
 const mode = process.env.STORYBOOK_ENV;
 
-// const V1_0_0_rc = [
-//   "Accordion",
-//   "ActionCard",
-//   "Button",
-//   "Checkbox",
-//   "Card",
-//   "FileUpload",
-//   "Footer",
-//   "Input",
-//   "Mainnav",
-//   "Masthead",
-//   "QuantityToggle",
-//   "Radio",
-//   "Sidenav",
-//   "Table",
-//   "Textarea"
-// ];
+const V1_0_0_rc = [
+  "Accordion",
+  "ActionCard",
+  "Button",
+  "Checkbox",
+  "Card",
+  "FileUpload",
+  "Footer",
+  "Input",
+  "Mainnav",
+  "Masthead",
+  "QuantityToggle",
+  "Radio",
+  "Sidenav",
+  "Table",
+  "Textarea",
+  "Tooltip",
+  "Modal",
+  "Badge", 
+  "Alert",
+  "Breadcrumb", 
+  "Toast",
+  "Dropdown",
+  "Spinner"
+];
 
-// const componentStories = () => {
-//   const productionStories = V1_0_0_rc.map(c => `../stories/components/${c}.stories.@(js|jsx|ts|tsx|mdx)`);
-//   if (mode === "development") {
-//     return ["../stories/components/*.stories.@(js|jsx|ts|tsx|mdx)"];
-//   } else return productionStories;
-// };
+const componentStories = () => {
+  const productionStories = V1_0_0_rc.map(c => `../stories/components/${c}.stories.@(js|jsx|ts|tsx|mdx)`);
+  if (mode === "development") {
+    return ["../stories/components/*.stories.@(js|jsx|ts|tsx|mdx)"];
+  } else return productionStories;
+};
 
 module.exports = {
   stories: [
     "../stories/frameworks/**/*.stories.@(js|jsx|ts|tsx|mdx)",
     "../stories/getting-started/**/*.stories.@(js|jsx|ts|tsx|mdx)",
-    "../stories/components/*.stories.@(js|jsx|ts|tsx|mdx)"
+    // "../stories/components/*.stories.@(js|jsx|ts|tsx|mdx)",
+    ...componentStories()
   ],
   addons: ["@storybook/addon-essentials", "@storybook/preset-scss"],
   framework: "@storybook/web-components",
