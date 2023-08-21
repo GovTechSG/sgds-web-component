26dcc68ebedaa26aa60e37e580bf7e18b4944858
jmestxr <tan_xing_rong@hotmail.com>
2023-08-21 09:43:47 +0800

    test(combobox): add test case for filterFunction

--- test/combo-box.test.ts
+++ test/combo-box.test.ts
@@ -40,7 +40,7 @@ describe("<sgds-combo-box>", () => {
     expect(inputHandler).to.have.been.calledOnce;
   });
 
-  it("combobox should update value of selected item", async () => {
+  it("should update value of selected item", async () => {
     const el = await fixture<SgdsComboBox>(html`<sgds-combo-box .menuList=${["Item #1", "Item #2"]}></sgds-combo-box>`);
 
     expect(el).to.have.attribute("value", undefined);
@@ -53,7 +53,7 @@ describe("<sgds-combo-box>", () => {
     expect(el).to.have.attribute("value", "Item #1");
   });
 
-  it("should not show any items in dropdown menu when there is no match", async () => {
+  it("should not show any items in dropdown menu when there is no match (for default filter)", async () => {
     const el = await fixture<SgdsComboBox>(
       html`<sgds-combo-box .menuList=${["apple", "pear", "orange", "apricot"]}></sgds-combo-box>`
     );
@@ -64,7 +64,7 @@ describe("<sgds-combo-box>", () => {
     expect(items?.length).to.equal(0);
   });
 
-  it("should filter the right items", async () => {
+  it("should filter the right items (for default filter)", async () => {
     const el = await fixture<SgdsComboBox>(
       html`<sgds-combo-box .menuList=${["apple", "pear", "orange", "apricot"]}></sgds-combo-box>`
     );
@@ -86,5 +86,19 @@ describe("<sgds-combo-box>", () => {
     const item = el.shadowRoot?.querySelector("sgds-dropdown-item");
     const itemVal = (item as SgdsDropdownItem).innerText;
     expect(itemVal).to.equal("apple");
+  });
+
+  it("should change filterFunction", async () => {
+    const el = await fixture<SgdsComboBox>(
+      html`<sgds-combo-box .menuList=${["apple", "pear", "orange", "apricot"]}></sgds-combo-box>`
+    );
+
+    // filterFunction that accepts all menuItem regardless of inputValue
+    el.filterFunction = (inputValue: string, menuItem: string) => true;
+
+    el.value = "test";
+    await el.updateComplete;
+    const items = el.shadowRoot?.querySelectorAll("sgds-dropdown-item");
+    expect(items?.length).to.equal(4);
   });
 });
