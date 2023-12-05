6b9d2330f195d6f7acaa03502da5c61a89cb16b1
jmestxr <tan_xing_rong@hotmail.com>
2023-12-05 10:51:36 +0800

    test(cypress): edit run_all.js script to kill all child processes upon exit

--- cypress/scripts/run_all.js
+++ cypress/scripts/run_all.js
@@ -1,28 +1,34 @@
-const { exec } = require("child_process");
+const { spawn } = require("child_process");
 
 console.log("\x1b[36m%s\x1b[0m", "Opening React, Angular, Vue apps");
 
-exec(`sh ${__dirname}/run_react.sh`, (error, stdout, stderr) => {
-  if (error) {
-    console.error("\x1b[31m%s\x1b[0m", "Error from React app");
-    console.error(error.message);
-    return;
-  }
-  console.log(stdout);
-});
-exec(`sh ${__dirname}/run_angular.sh`, (error, stdout, stderr) => {
-  if (error) {
-    console.error("\x1b[31m%s\x1b[0m", "Error from Angular app");
-    console.error(error.message);
-    return;
-  }
-  console.log(stdout);
-});
-exec(`sh ${__dirname}/run_vue.sh`, (error, stdout, stderr) => {
-  if (error) {
-    console.error("\x1b[31m%s\x1b[0m", "Error from Vue app");
-    console.error(error.message);
-    return;
-  }
-  console.log(stdout);
+// Function to run a shell command and return the child process
+function runCommand(command, name) {
+  const childProcess = spawn(command, { shell: true });
+
+  childProcess.stdout.on("data", data => {
+    console.log("\x1b[32m%s\x1b[0m", `Stdout from ${name} app: ${data}`);
+  });
+
+  childProcess.stderr.on("data", data => {
+    console.error("\x1b[31m%s\x1b[0m", `Stderr from ${name} app: ${data}`);
+  });
+
+  childProcess.on("error", error => {
+    console.error("\x1b[31m%s\x1b[0m", `Error from ${name} app: ${error.message}`);
+  });
+
+  return childProcess;
+}
+
+// Run the commands and store the child processes
+const reactProcess = runCommand(`sh ${__dirname}/run_react.sh`, "React");
+const angularProcess = runCommand(`sh ${__dirname}/run_angular.sh`, "Angular");
+const vueProcess = runCommand(`sh ${__dirname}/run_vue.sh`, "Vue");
+
+// Terminate all child processes when the main process exits
+process.on("exit", () => {
+  reactProcess.kill();
+  angularProcess.kill();
+  vueProcess.kill();
 });
