4a0c71eca816bd1cca91fab0a699a6adea1e8260
clukhei <kheiboardwarrior@gmail.com>
2024-04-22 09:16:09 +0800

    chore(*): lint fix

--- src/components/Sidenav/sgds-sidenav.ts
+++ src/components/Sidenav/sgds-sidenav.ts
@@ -53,7 +53,7 @@ export class SgdsSidenav extends SgdsElement {
       return;
     }
     items.forEach(item => {
-      // Covers all elements within sidenav-item 
+      // Covers all elements within sidenav-item
       if (!event.composedPath().includes(item)) {
         // Close all the items that didn't dispatch the event.
         item.active = false;
