c5f24c4d8ede45a221bc8e4b17a27c000f27488d
clukhei <kheiboardwarrior@gmail.com>
2024-05-16 22:10:09 +0800

    chore(*): revert lit-lab/react package

--- cypress/scripts/run_all.js
+++ cypress/scripts/run_all.js
@@ -1,6 +1,6 @@
 const { spawn } = require("child_process");
 
-console.log("\x1b[36m%s\x1b[0m", "Opening React, Angular, Vue apps");
+console.log("\x1b[36m%s\x1b[0m", "Opening React, Angular, Vue, Next apps");
 
 // Function to run a shell command and return the child process
 function runCommand(command, name) {
@@ -25,10 +25,12 @@ function runCommand(command, name) {
 const reactProcess = runCommand(`sh ${__dirname}/run_react.sh`, "React");
 const angularProcess = runCommand(`sh ${__dirname}/run_angular.sh`, "Angular");
 const vueProcess = runCommand(`sh ${__dirname}/run_vue.sh`, "Vue");
+const nextProcess = runCommand(`sh ${__dirname}/run_next.sh`, "Next");
 
 // Terminate all child processes when the main process exits
 process.on("exit", () => {
   reactProcess.kill();
   angularProcess.kill();
   vueProcess.kill();
+  nextProcess.kill();
 });
