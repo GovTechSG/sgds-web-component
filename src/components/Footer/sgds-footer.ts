359d81847217951a45990b99f8ecbbf0c701ecab
clukhei <kheiboardwarrior@gmail.com>
2024-05-27 11:55:33 +0800

    refactor(footer, masthead): remove hard coded svg for target blank from footer

--- src/components/Footer/sgds-footer.ts
+++ src/components/Footer/sgds-footer.ts
@@ -128,24 +128,8 @@ export class SgdsFooter extends SgdsElement {
                 <ul>
                   <li>
                     <a href="https://tech.gov.sg/report_vulnerability" target="_blank" rel="noopener noreferrer"
-                      >Report Vulnerability
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="16"
-                        height="16"
-                        fill="currentColor"
-                        class="bi bi-box-arrow-up-right"
-                        viewBox="0 0 16 16"
-                      >
-                        <path
-                          fill-rule="evenodd"
-                          d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z"
-                        />
-                        <path
-                          fill-rule="evenodd"
-                          d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z"
-                        /></svg
-                    ></a>
+                      >Report Vulnerability</a
+                    >
                   </li>
                   <li><a href=${this.privacyHref}>Privacy Statement</a></li>
                   <li><a href=${this.termsOfUseHref}>Terms of use</a></li>
