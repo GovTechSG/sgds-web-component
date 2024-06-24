bac48957291549e2d840862ab90eb0dff0aa87aa
clukhei <kheiboardwarrior@gmail.com>
2024-06-24 18:03:43 +0800

    refactor(mainnav, mainnavitem, mainnavdropdown): replacement of camelCase to kebab case in cssprop
    BREAKING CHANGE: --mainnav-borderBottom-width is replaced with --mainnav-border-bottom width for
    example.

--- src/components/Mainnav/sgds-mainnav-item.ts
+++ src/components/Mainnav/sgds-mainnav-item.ts
@@ -4,9 +4,10 @@ import mainnavItemStyle from "./mainnav-item.css";
 /**
  * @slot default - slot for SgdsMainnavItem element.
  *
- * @cssproperty --mainnav-item-theme-color - Hover and active color for mainnav items.
+ * @cssproperty --mainnav-item-theme-color - Hover and active color for mainnav items. By default, it follows --mainnav-theme-color
  * @cssproperty --mainnav-item-color - Text color of nav item.
- * @cssproperty --mainnav-item-borderBottom-width - border bottom width for hover and active state for nav item
+ * @cssproperty --mainnav-item-border-bottom-width - border bottom width for hover and active state for nav item
+ * @cssproperty --mainnav-item-disabled-opacity - The opacity of mainnav item at disabled state. By default, it follows --mainnav-disabled-opacity
  */
 export class SgdsMainnavItem extends LinkElement {
   static styles = [...LinkElement.styles, mainnavItemStyle];
