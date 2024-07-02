d6fd2a3ebf3c1b7f85db2e5e888bed3dc61ae1be
Tong Hau <tonghau_Lee@hive.gov.sg>
2024-07-03 02:21:01 +0800

    refactor(toast container): code refactor for toast container

--- src/components/Toast/sgds-toast-container.ts
+++ src/components/Toast/sgds-toast-container.ts
@@ -2,7 +2,6 @@ import { property } from "lit/decorators.js";
 import { classMap } from "lit/directives/class-map.js";
 import { html } from "lit/static-html.js";
 import SgdsElement from "../../base/sgds-element";
-import mergeDeep from "../../utils/mergeDeep";
 import toastContainerStyle from "./toast-container.css";
 /**
  * @summary ToastContainer is the container component to position `sgds-toast` in screen. When there is multiple toasts in the container, the toast components are stacked vertically.
@@ -10,6 +9,7 @@ import toastContainerStyle from "./toast-container.css";
  * @slot default - The slot for `sgds-toast` elements
  *
  * @cssproperty --toast-container-slot-elements-gap - The gap between multiple `sgds-toast` elements. Defaults to 0.5rem
+ * @cssproperty --toast-container-z-index - The z-index of the toast container, determining its stacking order relative to other elements.
  */
 export class SgdsToastContainer extends SgdsElement {
   static styles = [toastContainerStyle];
@@ -18,53 +18,18 @@ export class SgdsToastContainer extends SgdsElement {
   @property({ type: String, reflect: true }) position: ToastPosition;
 
   render() {
-    const generatePositionCssTokenObj = (position: ToastPosition) => {
-      if (!position) return {};
-      const arrayOfCssTokensObj = [
-        positionClasses[position].top,
-        positionClasses[position].start,
-        positionClasses[position].translate
-      ]
-        .filter(cssToken => !!cssToken)
-        .map(truthyCssToken => ({ [`${truthyCssToken}`]: true }));
-      return Object.assign({}, ...arrayOfCssTokensObj);
-    };
     return html`
       <div
-        class=${classMap(
-          mergeDeep(
-            {
-              "sgds toast-container": true
-            },
-            generatePositionCssTokenObj(this.position)
-          )
-        )}
+        class=${classMap({
+          "sgds toast-container": true,
+          [this.position]: true
+        })}
       >
         <slot></slot>
       </div>
     `;
   }
 }
-
-const positionClasses = {
-  "top-start": {
-    top: "top-0",
-    start: "start-50",
-    translate: "translate-middle-x"
-  },
-  "top-center": {
-    top: "top-0",
-    start: "start-50",
-    translate: "translate-middle-x"
-  },
-  "top-end": { top: "top-0", start: "end-0", translate: null },
-  "middle-start": { top: "top-50", start: "start-0", translate: "translate-middle-y" },
-  "middle-center": { top: "top-50", start: "start-50", translate: "translate-middle" },
-  "middle-end": { top: "top-50", start: "end-0", translate: "translate-middle-y" },
-  "bottom-start": { top: "bottom-0", start: "start-0", translate: null },
-  "bottom-center": { top: "bottom-0", start: "start-50", translate: "translate-middle-x" },
-  "bottom-end": { top: "bottom-0", start: "end-0", translate: null }
-};
 
 export type ToastPosition =
   | "top-start"
