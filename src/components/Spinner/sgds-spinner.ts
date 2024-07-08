36f0bfd7d3d940b603d5afa3764521367141503b
Tong Hau <tonghau_Lee@hive.gov.sg>
2024-07-09 01:39:07 +0800

    chore(spinner): clean up unused classes

--- src/components/Spinner/sgds-spinner.ts
+++ src/components/Spinner/sgds-spinner.ts
@@ -1,5 +1,4 @@
 import { property } from "lit/decorators.js";
-import { classMap } from "lit/directives/class-map.js";
 import { html } from "lit/static-html.js";
 import SgdsElement from "../../base/sgds-element";
 import spinnerStyle from "./spinner.css";
@@ -15,14 +14,7 @@ export class SgdsSpinner extends SgdsElement {
 
   render() {
     return html`
-      <div
-        class="
-            ${classMap({
-          [`spinner-${this.type}`]: this.type,
-          [`text-${this.color}`]: this.color
-        })}"
-        role="status"
-      >
+      <div class="spinner-${this.type}" role="status">
         <span class="sr-only">Loading...</span>
       </div>
     `;
