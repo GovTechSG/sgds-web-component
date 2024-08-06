7f65143958b587f5a61ec577eebbdb4481259387
Tong Hau <tonghau_Lee@hive.gov.sg>
2024-08-06 10:44:33 +0800

    feat(tooltip): add css variables and update click event

--- src/components/Tooltip/sgds-tooltip.ts
+++ src/components/Tooltip/sgds-tooltip.ts
@@ -11,10 +11,19 @@ import tooltipStyle from "./tooltip.css";
  * @summary Tooltips display more information when users hover over, focus on, or interact with an element.
  * @slot default - The element to target the tooltip to.
  *
- * @cssproperty --sgds-tooltip-max-width - Sets the tooltips max width. Default to 20rem.
+ * @cssproperty --sgds-tooltip-z-index - The z-index of tooltip
+ * @cssproperty --sgds-tooltip-max-width - The max width of tooltip
+ * @cssproperty --sgds-tooltip-padding-x - The left and right padding of tooltip
+ * @cssproperty --sgds-tooltip-padding-y - The top and bottom padding of tooltip
+ * @cssproperty --sgds-tooltip-font-size - The font size of tooltip
+ * @cssproperty --sgds-tooltip-color - The text color of tooltip
+ * @cssproperty --sgds-tooltip-bg - The background color of tooltip
+ * @cssproperty --sgds-tooltip-border-radius - The border radius of tooltip
+ * @cssproperty --sgds-tooltip-opacity - The opacity of tooltip
+ *
  */
 export class SgdsTooltip extends SgdsElement {
-  static styles = [tooltipStyle];
+  static styles = [...SgdsElement.styles, tooltipStyle];
 
   private myTooltip: Ref<HTMLElement> = createRef();
 
@@ -26,11 +35,10 @@ export class SgdsTooltip extends SgdsElement {
   /** The placement of tooltip relative to its target */
   @property({ type: String })
   placement: "top" | "bottom" | "left" | "right" = "top";
-  /** The method to invoke the tooltip. `hover focus` is the default value which allows tooltip to be triggered via mouse hover and keyboard focus. Add `tabindex=0 `for HTMLelements that are not tabbable. For `click` method, the tooltip is trigger via mouse clicking and it instantiates with a close button */
+  /** The method to invoke the tooltip. `hover focus` is the default value which allows tooltip to be triggered via mouse hover and keyboard focus. Add `tabindex=0 `for HTMLelements that are not tabbable. */
   @property({ type: String })
   trigger: "click" | "hover" | "focus" | "hover focus" = "hover focus";
 
-  private closableContainer: HTMLElement;
   /**@internal */
   @state()
   private popperConfig: Partial<PopperOptions>;
@@ -52,19 +60,11 @@ export class SgdsTooltip extends SgdsElement {
     //HTML method insertAdjacentText() is use to add the text content before close button but it requires a parent
     // Only way is to modify BsTooltip's title with "sanitize: false" to add the close button
     // When trigger is a "click", tooltipContainer will be created and modify BsTooltip's "title", adding a close button
+
+    // New update: Remove close button and when trigger is a "click", clicking/tapping on the body will hide the tooltip
     if (this.trigger === "click") {
-      this.closableContainer = document.createElement("div");
-      this.closableContainer.classList.add("d-flex");
-      this.closableContainer.classList.add("gap-4");
-      this.closableContainer.classList.add("text-start");
-      const closeBtn: HTMLButtonElement = document.createElement("button");
-      closeBtn.classList.add("btn-close");
-      closeBtn.classList.add("btn-close-white");
-      closeBtn.classList.add("mt-1");
-      closeBtn.setAttribute("aria-label", "Close");
-      this.closableContainer.appendChild(closeBtn);
-      this.closableContainer.insertAdjacentText("afterbegin", this.content);
-      this.shadowRoot.querySelector(".btn-close")?.addEventListener("click", () => this.hide());
+      document.querySelector("body").addEventListener("click", () => this.hide());
+      document.querySelector("body").addEventListener("touchstart", () => this.hide());
     }
     this.tooltipConfig = {
       popperConfig: (defaultConfig?: Partial<PopperOptions>) => {
@@ -81,9 +81,9 @@ export class SgdsTooltip extends SgdsElement {
       },
       placement: this.placement,
       trigger: this.trigger,
-      title: this.trigger === "click" ? this.closableContainer : this.content,
+      title: this.content,
       html: true,
-      sanitize: false, // to allow button element,
+      // sanitize: false, // to allow button element,
       container: this.shadowRoot.querySelector("div") // tooltip to appear inside the shadow root of sgds-tooltip instead of anywhere in the DOM, so that scoped styles can apply
     } as Partial<BsTooltip.Options>;
     this.bsTooltip = new Tooltip(this.myTooltip.value, this.tooltipConfig);
