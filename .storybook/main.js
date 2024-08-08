fd505c2253042af57a9e47f48376c08ca6c3a3e7
clukhei <kheiboardwarrior@gmail.com>
2024-08-08 15:27:39 +0800

    docs(*): add troublshotting

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,26 @@
+import remarkGfm from "remark-gfm";
 module.exports = {
   stories: [
     "../stories/frameworks/**/*.mdx",
     "../stories/getting-started/**/*.mdx",
-    "../stories/components/*.@(mdx|stories.@(js|jsx|ts|tsx))"
+    "../stories/components/*.@(mdx|stories.@(js|jsx|ts|tsx))",
+    "../stories/*.mdx"
   ],
-  addons: ["@storybook/addon-essentials", "@storybook/addon-a11y"],
+  addons: [
+    "@storybook/addon-essentials",
+    "@storybook/addon-a11y",
+    "@storybook/addon-mdx-gfm",
+    {
+      name: "@storybook/addon-docs",
+      options: {
+        mdxPluginOptions: {
+          mdxCompileOptions: {
+            remarkPlugins: [remarkGfm]
+          }
+        }
+      }
+    }
+  ],
   framework: "@storybook/web-components-vite",
   docs: {
     autodocs: "tag"
