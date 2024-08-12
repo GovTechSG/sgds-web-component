88035e48ec0e973522d99e4a372ad6c2ea7fb63d
clukhei <kheiboardwarrior@gmail.com>
2024-08-12 11:34:46 +0800

    docs(*): additional mdx wip

---
+++ stories/templates/ActionCard/additional.stories.js
@@ -0,0 +1,29 @@
+const CardAsRadioTemplate = args =>
+    html`
+      <form> 
+      <sgds-radio-group>
+        <sgds-action-card type="radio" name="apple">
+          <span slot="card-subtitle">Laptop</span>
+          <span slot="card-title">Apple</span>
+          <span slot="card-text">Macbook Pro M1</span>
+        </sgds-action-card>
+        <sgds-action-card type="radio" name="microsoft">
+          <span slot="card-subtitle">Laptop</span>
+          <span slot="card-title">Microsoft</span>
+          <span slot="card-text">Microsoft Surface Pro</span>
+        </sgds-action-card>
+        <sgds-action-card type="radio" name="acer">
+          <span slot="card-subtitle">Laptop</span>
+          <span slot="card-title">Acer</span>
+          <span slot="card-text">Aspire 5</span>
+        </sgds-action-card>
+        </sgds-radio-group>
+        </form>
+    `;
+  
+export const ActionCardAsRadio = {
+    render: CardAsRadioTemplate.bind({}),
+    name: "Action Card as radio",
+    args: {},
+    parameters: {}
+}
