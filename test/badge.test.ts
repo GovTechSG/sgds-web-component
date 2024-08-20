8d2441319b9b334d6c6b565230695f6b9f0d44e8
Seethamsetty Sai Utkarsh <saiutkarsh33@gmail.com>
2024-08-20 14:43:34 +0800

    test(badge): add unit tests for 'show' and 'outined' props

--- test/badge.test.ts
+++ test/badge.test.ts
@@ -31,4 +31,32 @@ describe("SgdsBadge component", () => {
   });
 });
 
+describe("SgdsBadge component", () => {
+  it("should render with the 'outlined' class when outlined is true", async () => {
+    const el = await fixture<SgdsBadge>(html`<sgds-badge outlined></sgds-badge>`);
+    await elementUpdated(el);
+    expect(el.shadowRoot?.querySelector(".badge")).to.have.class("outlined");
+  });
+
+  it("should not render with the 'outlined' class when outlined is false", async () => {
+    const el = await fixture<SgdsBadge>(html`<sgds-badge></sgds-badge>`);
+    await elementUpdated(el);
+    expect(el.shadowRoot?.querySelector(".badge")).to.not.have.class("outlined");
+  });
+});
+
+describe("SgdsBadge component", () => {
+  it("should render when show is true", async () => {
+    const el = await fixture<SgdsBadge>(html`<sgds-badge show></sgds-badge>`);
+    await elementUpdated(el);
+    expect(el.shadowRoot?.querySelector(".badge")).to.exist;
+  });
+
+  it("should not render when show is false", async () => {
+    const el = await fixture<SgdsBadge>(html`<sgds-badge .show=${false}></sgds-badge>`);
+    await elementUpdated(el);
+    expect(el.shadowRoot?.querySelector(".badge")).to.not.exist;
+  });
+});
+
 
