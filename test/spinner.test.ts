4c0739e98fff79106ff99d3d346742270c329fcc
Tong Hau <tonghau_Lee@hive.gov.sg>
2024-08-27 11:41:42 +0800

    feat(spinner): removal of spinner type prop and rename color prop to variant
    BREAKING CHANGE: Users who previously use the props will find that it no longer applies and will
    have styling breaks.

--- test/spinner.test.ts
+++ test/spinner.test.ts
@@ -4,21 +4,21 @@ import { html } from "lit";
 import type { SgdsSpinner } from "../src/components";
 
 describe("<sgds-spinner>", () => {
-  it("should have default class of 'spinner-border' if no 'type' attribute define", async () => {
+  it("should have default class of 'spinner-md' if no size attribute define", async () => {
     const el = await fixture<SgdsSpinner>(html`<sgds-spinner></sgds-spinner>`);
     const base = el.shadowRoot?.querySelector<HTMLElement>('[role="status"]');
-    expect(base).to.have.class("spinner-border");
+    expect(base).to.have.class("spinner-md");
   });
 
-  it("should have class of 'spinner-border' if type='border'", async () => {
-    const el = await fixture(html`<sgds-spinner type="border"></sgds-spinner>`);
+  it("should have class of 'spinner-sm' if size='sm'", async () => {
+    const el = await fixture(html`<sgds-spinner size="sm"></sgds-spinner>`);
     const base = el.shadowRoot?.querySelector<HTMLElement>('[role="status"]');
-    expect(base).to.have.class("spinner-border");
+    expect(base).to.have.class("spinner-sm");
   });
 
-  it("should have class of 'spinner-grow' if type='grow'", async () => {
-    const el = await fixture(html`<sgds-spinner type="grow"></sgds-spinner>`);
+  it("should have class of 'spinner-lg' if size='lg'", async () => {
+    const el = await fixture(html`<sgds-spinner size="lg"></sgds-spinner>`);
     const base = el.shadowRoot?.querySelector<HTMLElement>('[role="status"]');
-    expect(base).to.have.class("spinner-grow");
+    expect(base).to.have.class("spinner-lg");
   });
 });
