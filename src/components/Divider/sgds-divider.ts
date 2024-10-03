f9ad92b529a18319eb87858342b2ea5dbfecc6e9
Lu Khei Chong <kheiboardwarrior@gmail.com>
2024-10-03 14:12:17 +0800

    test(divider): add ut

--- src/components/Divider/sgds-divider.ts
+++ src/components/Divider/sgds-divider.ts
@@ -1,7 +1,9 @@
 import { property } from "lit/decorators.js";
 import SgdsElement from "../../base/sgds-element";
 import dividerStyles from "./divider.css";
-
+/**
+ * @summary A divider is a thin line that groups content in lists and layouts. They bring clarity to a layout by grouping and dividing content in close proximity.
+ */
 export class SgdsDivider extends SgdsElement {
   static styles = [dividerStyles];
   /** Sets the orientation of divider to vertical. Defaults to horizontal */
