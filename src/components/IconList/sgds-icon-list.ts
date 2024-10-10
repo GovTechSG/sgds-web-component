d43498605494e1d13e5c5d6c926b634766e91b53
Lu Khei Chong <kheiboardwarrior@gmail.com>
2024-10-10 11:53:08 +0800

    feat(IconList): add size prop

--- src/components/IconList/sgds-icon-list.ts
+++ src/components/IconList/sgds-icon-list.ts
@@ -2,6 +2,7 @@ import SgdsElement from "../../base/sgds-element";
 import { html } from "lit";
 import iconListStyles from "./icon-list.css";
 import { property } from "lit/decorators.js";
+import { classMap } from "lit/directives/class-map.js";
 /**
  * @summary A IconList can be used to display content related to the same topic. Each list item begins an icon.
  *
@@ -10,11 +11,14 @@ import { property } from "lit/decorators.js";
 export class SgdsIconList extends SgdsElement {
   static styles = [...SgdsElement.styles, iconListStyles];
 
+  /** Sets the aria-role of the sgds-icon-list */
   @property({ type: String, reflect: true }) role = "list";
+  /** The size of icon list. Changes the font-size the list items */
+  @property({ type: String, reflect: true }) size: "sm" | "md" | "lg" = "md";
 
   render() {
     return html`
-      <div>
+      <div class=${classMap({ [this.size]: this.size })}>
         <slot></slot>
       </div>
     `;
--- test/icon-list.test.ts
+++ test/icon-list.test.ts
@@ -6,10 +6,14 @@ import "./sgds-web-component";
 describe("<sgds-icon-list>", () => {
   it("matches shadowDom semantically", async () => {
     const el = await fixture<SgdsIconList>(html`<sgds-icon-list></sgds-icon-list>`);
-    assert.shadowDom.equal(el, `<div><slot></slot></div>`);
+    assert.shadowDom.equal(el, `<div class="md"><slot></slot></div>`);
   });
   it("role=list attribute is added to the custom element", async () => {
     const el = await fixture<SgdsIconList>(html`<sgds-icon-list></sgds-icon-list>`);
     expect(el.getAttribute("role")).to.equal("list");
+  });
+  it("size prop is forwarded down to the class attribute of shadow dom ", async () => {
+    const el = await fixture<SgdsIconList>(html`<sgds-icon-list size="sm"></sgds-icon-list>`);
+    expect(el.shadowRoot?.querySelector("div")?.classList.value).to.contain("sm");
   });
 });
