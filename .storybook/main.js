e30236fe6e06972561e9bbd5d38325e7d6669b27
Lu Khei Chong <kheiboardwarrior@gmail.com>
2024-10-28 17:09:16 +0800

    chore(*): update branch from lab

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,7 +3,18 @@ module.exports = {
   stories: [
     "../stories/frameworks/**/*.mdx",
     "../stories/getting-started/**/*.mdx",
-    "../stories/components/*.@(mdx|stories.@(js|jsx|ts|tsx))",
+    // "../stories/components/*.@(mdx|stories.@(js|jsx|ts|tsx))",
+    "../stories/components/Accordion.@(mdx|stories.@(js|jsx|ts|tsx))",
+    "../stories/components/Alert.@(mdx|stories.@(js|jsx|ts|tsx))",
+    "../stories/components/Button.@(mdx|stories.@(js|jsx|ts|tsx))",
+    // "../stories/components/Checkbox.@(mdx|stories.@(js|jsx|ts|tsx))",
+    "../stories/components/Input.@(mdx|stories.@(js|jsx|ts|tsx))",
+    "../stories/components/Radio.@(mdx|stories.@(js|jsx|ts|tsx))",
+    "../stories/components/Masthead.@(mdx|stories.@(js|jsx|ts|tsx))",
+    "../stories/components/Progress.@(mdx|stories.@(js|jsx|ts|tsx))",
+    "../stories/components/Spinner.@(mdx|stories.@(js|jsx|ts|tsx))",
+    "../stories/components/Stepper.@(mdx|stories.@(js|jsx|ts|tsx))",
+    "../stories/components/Switch.@(mdx|stories.@(js|jsx|ts|tsx))",
     "../stories/*.mdx"
   ],
   addons: [
