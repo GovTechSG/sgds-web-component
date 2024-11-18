a987aecb47bacd990a7f2f296f5b54bf1193b427
Tong Hau <tonghau_Lee@hive.gov.sg>
2024-11-19 03:50:45 +0800

    chore(close button): use icon component in close button

--- src/internals/CloseButton/sgds-close-button.ts
+++ src/internals/CloseButton/sgds-close-button.ts
@@ -1,17 +1,25 @@
-import { property } from "lit/decorators.js";
-import SgdsElement from "../../base/sgds-element";
 import { html } from "lit";
-import { ifDefined } from "lit/directives/if-defined.js";
-import closeButtonStyles from "./close-button.css";
+import { property } from "lit/decorators.js";
+import { ScopedElementsMixin } from "@open-wc/scoped-elements";
+import SgdsElement from "../../base/sgds-element";
 import { classMap } from "lit/directives/class-map.js";
+import { ifDefined } from "lit/directives/if-defined.js";
+import SgdsIcon from "../../components/Icon/sgds-icon";
+import closeButtonStyles from "./close-button.css";
 /**
  * @summary Close button for closing actions. Used in Modal, Drawer, Alert and Toast.
  *
  * @cssprop --sgds-close-btn-border-radius - The border radius of close button border
  *
  */
-export class SgdsCloseButton extends SgdsElement {
+export class SgdsCloseButton extends ScopedElementsMixin(SgdsElement) {
   static styles = [closeButtonStyles];
+  /**@internal */
+  static get scopedElements() {
+    return {
+      "sgds-icon": SgdsIcon
+    };
+  }
 
   @property({ type: String }) ariaLabel = "Close button";
 
@@ -40,7 +48,9 @@ export class SgdsCloseButton extends SgdsElement {
         })}
         aria-label=${ifDefined(this.ariaLabel)}
         @click=${this._handleClick}
-      ></button>
+      >
+        <sgds-icon name="cross"></sgds-icon>
+      </button>
     `;
   }
 }
