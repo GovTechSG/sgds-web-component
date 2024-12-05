b0ebe61252f82d3eba9e640c77524cf039ffcf30
Lu Khei Chong <kheiboardwarrior@gmail.com>
2024-12-05 11:20:12 +0800

    feat(breadcrumb): stylings

--- src/components/Breadcrumb/sgds-breadcrumb-item.ts
+++ src/components/Breadcrumb/sgds-breadcrumb-item.ts
@@ -1,10 +1,7 @@
 import { html } from "lit";
 import { property } from "lit/decorators.js";
-import { classMap } from "lit/directives/class-map.js";
-import { ifDefined } from "lit/directives/if-defined.js";
-import SgdsElement from "../../base/sgds-element";
+import SgdsLink from "../Link/sgds-link";
 import breadcrumbItemStyle from "./breadcrumb-item.css";
-import anchorStyles from "../../styles/anchor.css";
 /**
  * @summary Breadcrumb Item are navigational links used in Breadcrumb component
  *
@@ -15,39 +12,15 @@ import anchorStyles from "../../styles/anchor.css";
  * @csspart label -The label of the breadcrumb item. It is either a span or anchor element depending on href attribute
  *
  */
-export class SgdsBreadcrumbItem extends SgdsElement {
-  static styles = [...SgdsElement.styles, anchorStyles, breadcrumbItemStyle];
+export class SgdsBreadcrumbItem extends SgdsLink {
+  static styles = [...SgdsLink.styles, breadcrumbItemStyle];
   /** Specifies the url path of the breadcrumb-item. When defined, the breadcrumb-items is a anchor element. When not defined, indicates that the breadcrumb item is active. In such cases, a span element is rendered. */
-  @property({ type: String, reflect: true }) href: string;
-  /** Tells the browser where to open the link. Only used when `href` is set. */
-  @property() target: "_blank" | "_parent" | "_self" | "_top";
-
-  /** The `rel` attribute to use on the link. Only used when `href` is set. */
-  @property() rel = "noreferrer noopener";
+  @property({ type: String, reflect: true }) active: string;
 
   render() {
-    const isLink = this.href ? true : false;
     return html`
-      <div
-        part="base"
-        class=${classMap({
-          "breadcrumb-item": true,
-          active: !isLink
-        })}
-      >
-        ${isLink
-          ? html`
-              <a
-                part="label"
-                href="${this.href}"
-                rel=${ifDefined(this.target ? this.rel : undefined)}
-                target=${ifDefined(this.target)}
-                ><slot></slot
-              ></a>
-            `
-          : html`<span part="label"><slot></slot></span>`}
-        <slot name="separator" class="breadcrumb-item__separator" aria-hidden="true"></slot>
-      </div>
+      <slot class="nav-link"></slot>
+      <slot name="separator" class="breadcrumb-item__separator" aria-hidden="true"></slot>
     `;
   }
 }
