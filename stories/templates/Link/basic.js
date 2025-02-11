a901a6b54c3ac37b7374e02aae45f955121b7b02
Lu Khei Chong <kheiboardwarrior@gmail.com>
2025-02-11 14:33:44 +0800

    docs(link): add stories

--- stories/templates/Link/basic.js
+++ stories/templates/Link/basic.js
@@ -1,6 +1,11 @@
 import { html } from "lit-html";
+import { ifDefined } from "lit/directives/if-defined.js";
 
-export const Template = args => html`<sgds-link></sgds-link>`;
+export const Template = args => html`
+  <sgds-link size=${ifDefined(args.size)} variant=${ifDefined(args.variant)}>
+    <a href="#">Link</a>
+  </sgds-link>
+`;
 
 export const args = {};
 
