5276655887724a7422245ec38ec5309b37bc829e
Lu Khei Chong <kheiboardwarrior@gmail.com>
2025-02-13 14:59:55 +0800

    docs(sidenav): add stories

--- stories/templates/Sidenav/basic.js
+++ stories/templates/Sidenav/basic.js
@@ -1,73 +1,43 @@
 import { html } from "lit-html";
-import { ifDefined } from "lit/directives/if-defined.js";
 
-export const Template = ({
-  alwaysOpen,
-  active,
-  href,
-  activeSNL,
-  hrefSNL,
-  disabledSNL,
-  disabledSNI,
-  sticky,
-  target
-}) => {
+export const Template = ({ active, activeSNL, activeSNIAsLink, disabledSNL, disabledSNI, sticky }) => {
   return html`
-    <sgds-sidenav ?alwaysOpen=${alwaysOpen} ?sticky=${sticky}>
-      <sgds-sidenav-item ?active=${active} href=${ifDefined(href)} ?disabled=${disabledSNI}>
-        <span slot="title">SideNav Item #1 (control by Argstable) </span>
-        <sgds-sidenav-link
-          href=${ifDefined(hrefSNL)}
-          ?active=${activeSNL}
-          ?disabled=${disabledSNL}
-          target=${ifDefined(target)}
-          >sgds-sidenav-link (control by Argstable)</sgds-sidenav-link
-        >
-        <sgds-sidenav-link href="#" disabled>sgds-sidenav-link</sgds-sidenav-link>
-        <sgds-sidenav-link href="#">sgds-sidenav-link</sgds-sidenav-link>
+    <sgds-sidenav ?sticky=${sticky}>
+      <sgds-sidenav-item ?active=${active} ?disabled=${disabledSNI}>
+          <span slot="title">SideNavItem L1 label (menu) </span>
+          <sgds-sidenav-link
+            ?active=${activeSNL}
+            ?disabled=${disabledSNL}
+            ><a href="#">SidenavLink L2 label</a></sgds-sidenav-link
+          >
+          <sgds-sidenav-link> 
+            <a href="#">SidenavLink L2 label</a>
+          </sgds-sidenav-link>
+          <sgds-sidenav-link>
+            <a href="#">SidenavLink L2 label</a>
+          </sgds-sidenav-link>
+          <sgds-sidenav-item>
+            <span slot="title">
+            SidenavItem L2 label
+            </span>
+            <sgds-sidenav-link>
+              <a href="#"> SidenavLink L3 label</a>
+            </sgds-sidenav-link>
+            <sgds-sidenav-link>
+              <a href="#">SidenavLink L3 label</a>
+            </sgds-sidenav-link>
+          </sgds-sidenav-item>
       </sgds-sidenav-item>
       <sgds-sidenav-item>
-        <span slot="title">SideNav Item #2</span>
-        <span slot="icon">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            class="bi bi-layers-fill"
-            viewBox="0 0 16 16"
-          >
-            <path
-              d="M7.765 1.559a.5.5 0 0 1 .47 0l7.5 4a.5.5 0 0 1 0 .882l-7.5 4a.5.5 0 0 1-.47 0l-7.5-4a.5.5 0 0 1 0-.882l7.5-4z"
-            />
-            <path
-              d="m2.125 8.567-1.86.992a.5.5 0 0 0 0 .882l7.5 4a.5.5 0 0 0 .47 0l7.5-4a.5.5 0 0 0 0-.882l-1.86-.992-5.17 2.756a1.5 1.5 0 0 1-1.41 0l-5.17-2.756z"
-            />
-          </svg>
-        </span>
-        <sgds-sidenav-link href="#">sgds-sidenav-link</sgds-sidenav-link>
-        <sgds-sidenav-link href="#">sgds-sidenav-link</sgds-sidenav-link>
-        <sgds-sidenav-link href="#">sgds-sidenav-link</sgds-sidenav-link>
+        <span slot="title">SideNavItem L1 label (menu)</span>
+        <sgds-sidenav-link> 
+            <a href="#">SidenavLink L2 label</a>
+          <sgds-sidenav-link>
+            <a href="#">SidenavLink L2 label</a>
+          </sgds-sidenav-link>
       </sgds-sidenav-item>
-      <sgds-sidenav-item href="#">
-        <span slot="title">SideNav Item #3</span>
-        <span slot="icon">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            class="bi bi-layers-fill"
-            viewBox="0 0 16 16"
-          >
-            <path
-              d="M7.765 1.559a.5.5 0 0 1 .47 0l7.5 4a.5.5 0 0 1 0 .882l-7.5 4a.5.5 0 0 1-.47 0l-7.5-4a.5.5 0 0 1 0-.882l7.5-4z"
-            />
-            <path
-              d="m2.125 8.567-1.86.992a.5.5 0 0 0 0 .882l7.5 4a.5.5 0 0 0 .47 0l7.5-4a.5.5 0 0 0 0-.882l-1.86-.992-5.17 2.756a1.5 1.5 0 0 1-1.41 0l-5.17-2.756z"
-            />
-          </svg>
-        </span>
+      <sgds-sidenav-item ?active=${activeSNIAsLink}>
+        <a href="#">SidenavItem L1 label (link)</a>
       </sgds-sidenav-item>
     </sgds-sidenav>
   `;
