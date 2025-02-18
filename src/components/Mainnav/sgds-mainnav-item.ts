8e6b7eb8e28d925f1492de3897904328bf8a62e4
Tong Hau <tonghau_Lee@hive.gov.sg>
2025-02-18 17:17:41 +0800

    chore(mainnav item): change link item to slot

--- src/components/Mainnav/sgds-mainnav-item.ts
+++ src/components/Mainnav/sgds-mainnav-item.ts
@@ -1,12 +1,46 @@
-import LinkElement from "../../base/link-element";
+import { html } from "lit";
+import { property } from "lit/decorators.js";
+import { watch } from "../../utils/watch";
+import SgdsElement from "../../base/sgds-element";
 import mainnavItemStyle from "./mainnav-item.css";
 
 /**
  * @slot default - slot for SgdsMainnavItem element.
  *
  *  */
-export class SgdsMainnavItem extends LinkElement {
-  static styles = [...LinkElement.styles, mainnavItemStyle];
+export class SgdsMainnavItem extends SgdsElement {
+  static styles = [...SgdsElement.styles, mainnavItemStyle];
+
+  /** when true, sets the active stylings of .nav-link */
+  @property({ type: Boolean })
+  active = false;
+
+  /** Disables the SgdsMainnavItem */
+  @property({ type: Boolean, reflect: true })
+  disabled = false;
+
+  @watch("disabled")
+  _handleDisabled() {
+    this.setAttribute("aria-disabled", `${this.disabled}`);
+  }
+
+  private _handleSlotChange(e: Event) {
+    const anchorItems = (e.target as HTMLSlotElement)
+      .assignedElements({ flatten: true })
+      .filter(item => item.tagName.toLowerCase() === "a");
+
+    if (anchorItems.length > 1) {
+      return console.error("More than one anchor tag is added to sgds-sidenav-item");
+    } else if (anchorItems.length === 1) {
+      const anchor = anchorItems[0] as HTMLAnchorElement;
+      this.disabled && anchor.setAttribute("tabindex", "-1");
+      this.active && anchor.setAttribute("aria-current", "true");
+    }
+  }
+
+  render() {
+    return html`<slot @slotchange=${this._handleSlotChange}></slot>`;
+  }
 }
 
 export default SgdsMainnavItem;
