93ded373c660838ad6218b6ea27c411421a70792
Tong Hau <tonghau_Lee@hive.gov.sg>
2025-04-01 01:39:38 +0800

    chore(icon list): update docs

---
+++ stories/templates/IconList/additional.stories.js
@@ -0,0 +1,31 @@
+import { html } from "lit-html";
+
+const SizeTemplate = () => {
+  return html`
+    <div class="d-flex-row">
+      <sgds-icon-list size="sm">
+        <div role="listitem"><sgds-icon size="sm" name="placeholder"></sgds-icon>item 1</div>
+        <div role="listitem"><sgds-icon size="sm" name="placeholder"></sgds-icon>item 2</div>
+        <div role="listitem"><sgds-icon size="sm" name="placeholder"></sgds-icon>item 3</div>
+      </sgds-icon-list>
+      <sgds-icon-list>
+        <div role="listitem"><sgds-icon size="md" name="placeholder"></sgds-icon>item 1</div>
+        <div role="listitem"><sgds-icon size="md" name="placeholder"></sgds-icon>item 2</div>
+        <div role="listitem"><sgds-icon size="md" name="placeholder"></sgds-icon>item 3</div>
+      </sgds-icon-list>
+      <sgds-icon-list size="lg">
+        <div role="listitem"><sgds-icon name="placeholder"></sgds-icon>item 1</div>
+        <div role="listitem"><sgds-icon name="placeholder"></sgds-icon>item 2</div>
+        <div role="listitem"><sgds-icon name="placeholder"></sgds-icon>item 3</div>
+      </sgds-icon-list>
+    </div>
+  `;
+};
+
+export const Sizes = {
+  render: SizeTemplate.bind({}),
+  name: "Sizes",
+  args: {},
+  parameters: {},
+  tags: ["!dev"]
+};
--- stories/templates/IconList/basic.js
+++ stories/templates/IconList/basic.js
@@ -1,6 +1,12 @@
 import { html } from "lit-html";
 
-export const Template = args => html`<sgds-icon-list></sgds-icon-list>`;
+export const Template = args => html`
+  <sgds-icon-list>
+    <div role="listitem"><sgds-icon size="sm" name="placeholder"></sgds-icon>item 1</div>
+    <div role="listitem"><sgds-icon size="sm" name="placeholder"></sgds-icon>item 2</div>
+    <div role="listitem"><sgds-icon size="sm" name="placeholder"></sgds-icon>item 3</div>
+  </sgds-icon-list>
+`;
 
 export const args = {};
 
