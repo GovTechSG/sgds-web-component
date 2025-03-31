1d89b2b55898dffc40bb5a3ed68aa85cebea9d6b
Tong Hau <tonghau_Lee@hive.gov.sg>
2025-04-01 01:40:32 +0800

    chore(table): update docs

--- stories/templates/Table/basic.js
+++ stories/templates/Table/basic.js
@@ -4,12 +4,6 @@ import { ifDefined } from "lit/directives/if-defined.js";
 export const Template = args => {
   return html`
     <sgds-table
-      ?striped=${args.striped}
-      ?bordered=${args.bordered}
-      ?borderless=${args.borderless}
-      ?hover=${args.hover}
-      size=${ifDefined(args.size)}
-      variant=${ifDefined(args.variant)}
       responsive=${ifDefined(args.responsive)}
       .rowHeader=${["#", "First Names", "Last Name", "Username"]}
       .tableData=${[
@@ -17,8 +11,6 @@ export const Template = args => {
         ["2", "Jane", "Doe", "@janedoe"],
         ["3", "Bob", "Smith", "@bobsmith"]
       ]}
-      ?sort=${args.sort}
-      ?removableSort=${args.removableSort}
     >
     </sgds-table>
   `;
