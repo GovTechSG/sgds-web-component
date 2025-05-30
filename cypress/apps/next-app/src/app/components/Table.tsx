7df93e5d4a3ffb5d342dac726ff42441533696d0
Amanda Chan <amanda.chan@activate.sg>
2025-05-30 19:43:25 +0800

    chore(table): updated samples in next and react apps

--- cypress/apps/next-app/src/app/components/Table.tsx
+++ cypress/apps/next-app/src/app/components/Table.tsx
@@ -1,19 +1,109 @@
+"use client";
+
 import SgdsTable from "@govtechsg/sgds-web-component/react/table/index.js";
+import "@govtechsg/sgds-web-component/react/button/index.js";
+import "@govtechsg/sgds-web-component/react/icon-button/index.js";
+import "@govtechsg/sgds-web-component/react/badge/index.js";
+import { useEffect } from "react";
 
 export const Table = () => {
+  const rowHeader = [
+    {
+      key: "first-name",
+      value: "First Name"
+    },
+    {
+      key: "last-name",
+      value: "Last Name",
+      render: {
+        id: "row-link",
+        type: "link"
+      }
+    },
+    {
+      key: "email",
+      value: "Email",
+      render: {
+        type: "badge",
+        props: {
+          variant: "success"
+        }
+      }
+    },
+    {
+      key: "buttons",
+      value: "Button",
+      render: {
+        type: "button",
+        props: {
+          variant: "outline",
+          id: "stepper-btn-next",
+          type: "reset"
+        }
+      }
+    },
+    {
+      key: "action",
+      value: "icon-button",
+      render: [
+        {
+          type: "icon-button",
+          props: {
+            variant: "outline",
+            id: "stepper-btn-next",
+            name: "plus"
+          }
+        },
+        {
+          type: "icon-button",
+          props: {
+            variant: "outline",
+            id: "stepper-btn-next",
+            name: "plus"
+          }
+        }
+      ]
+    }
+  ];
+
+  const tableData = [
+    {
+      email: "@johndoe",
+      "first-name": "John",
+      "last-name": "Doe",
+      buttons: "click"
+    },
+    {
+      email: "@johndoe",
+      "first-name": "John",
+      "last-name": "Doe",
+      buttons: "click"
+    }
+  ];
+
+  const columnHeader = ["First Names", "Last Name", "Username"];
+
+  const handleTableLink = (e: unknown) => {
+    console.log("handleTableLink", e);
+  };
+
+  useEffect(() => {
+    document.addEventListener("sgds-table-click-row-link", handleTableLink);
+
+    return () => {
+      document.removeEventListener("sgds-table-click-row-link", handleTableLink);
+    };
+  });
+
   return (
     <>
-      <SgdsTable
-        rowHeader={["#", "First Names", "Last Name", "Username"]}
-        tableData={[
-          ["1", "John", "Doe", "@johndoe"],
-          ["2", "Jane", "Doe", "@janedoe"],
-          ["3", "Bob", "Smith", "@bobsmith"]
-        ]}
-      />
-      <SgdsTable
+      <SgdsTable rowHeader={rowHeader} tableData={tableData} />
+      <SgdsTable headerPosition="vertical" columnHeader={columnHeader} tableData={tableData} />
+      <SgdsTable headerPosition="both" rowHeader={rowHeader} columnHeader={columnHeader} tableData={tableData} />
+
+      {/* <SgdsTable
         headerPosition="vertical"
-        columnHeader={["#", "First Names", "Last Name", "Username"]}
+        
         tableData={[
           [1, "John", "Doe", "@johndoe"],
           [2, "Jane", "Doe", "@janedoe"],
@@ -33,7 +123,7 @@ export const Table = () => {
         ["149", "336", "792", "129"],
         ["133", "302", "508", "66"]
         ]'
-      />
+      /> */}
     </>
   );
 };
--- cypress/apps/react-app/src/components/Table.jsx
+++ cypress/apps/react-app/src/components/Table.jsx
@@ -1,14 +1,100 @@
-import SgdsTable from "@govtechsg/sgds-web-component/react/table";
+// import "@govtechsg/sgds-web-component/react/table/index.js";
+// import "@govtechsg/sgds-web-component/components/Badge";
+// import "@govtechsg/sgds-web-component/components/Button";
+// import "@govtechsg/sgds-web-component/components/IconButton";
+import { useEffect } from "react";
+import SgdsTable from "@govtechsg/sgds-web-component/react/table/index.js";
 
 export const Table = () => {
+  const rowHeader = [
+    {
+      key: "first-name",
+      value: "First Name"
+    },
+    {
+      key: "last-name",
+      value: "Last Name",
+      render: {
+        id: "row-link",
+        type: "link"
+      }
+    },
+    {
+      key: "email",
+      value: "Email",
+      render: {
+        type: "badge",
+        props: {
+          variant: "success"
+        }
+      }
+    },
+    {
+      key: "buttons",
+      value: "Button",
+      render: {
+        type: "button",
+        props: {
+          variant: "outline",
+          id: "stepper-btn-next",
+          type: "reset"
+        }
+      }
+    },
+    {
+      key: "action",
+      value: "icon-button",
+      render: [
+        {
+          type: "icon-button",
+          props: {
+            variant: "outline",
+            id: "stepper-btn-next",
+            name: "plus"
+          }
+        },
+        {
+          type: "icon-button",
+          props: {
+            variant: "outline",
+            id: "stepper-btn-next",
+            name: "plus"
+          }
+        }
+      ]
+    }
+  ];
+
+  const tableData = [
+    {
+      email: "@johndoe",
+      "first-name": "John",
+      "last-name": "Doe",
+      buttons: "click"
+    },
+    {
+      email: "@johndoe",
+      "first-name": "John",
+      "last-name": "Doe",
+      buttons: "click"
+    }
+  ];
+
+  const handleTableLink = e => {
+    console.log("handleTableLink", e);
+  };
+
+  useEffect(() => {
+    document.addEventListener("sgds-table-click-row-link", handleTableLink);
+
+    return () => {
+      document.removeEventListener("sgds-table-click-row-link", handleTableLink);
+    };
+  });
+
   return (
-    <SgdsTable
-      rowheader='["#", "First Names", "Last Name", "Username"]'
-      tabledata='[
-        ["1", "John", "Doe", "@johndoe"],
-        ["2", "Jane", "Doe", "@janedoe"],
-        ["3", "Bob", "Smith", "@bobsmith"]
-      ]'
-    />
+    <>
+      <SgdsTable rowHeader={rowHeader} tableData={tableData}></SgdsTable>
+    </>
   );
 };
