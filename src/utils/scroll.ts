d316aee7cdf3b1a2ebfb5fd97828636e26f9d61a
Jiaxi Tang <jt.jiaxitang@gmail.com>
2025-07-15 15:48:36 +0800

    fix(modal): fix scroll lock and padding/width in fullscreen

--- src/utils/scroll.ts
+++ src/utils/scroll.ts
@@ -8,7 +8,7 @@ const locks = new Set();
  */
 export function lockBodyScrolling(lockingEl: HTMLElement) {
   locks.add(lockingEl);
-  document.body.classList.add("sl-scroll-lock");
+  document.body.style.overflow = "hidden";
 }
 
 /**
@@ -52,6 +52,6 @@ export function unlockBodyScrolling(lockingEl: HTMLElement) {
   locks.delete(lockingEl);
 
   if (locks.size === 0) {
-    document.body.classList.remove("sl-scroll-lock");
+    document.body.style.overflow = "visible";
   }
 }
