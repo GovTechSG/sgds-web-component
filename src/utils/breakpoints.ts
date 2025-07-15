70efcf0bb417a8c7bc128c25002d0828f24d4909
Tong Hau <tonghau_Lee@hive.gov.sg>
2025-07-15 18:09:20 +0800

    style(mainnav): update based on grid breakpoint

--- src/utils/breakpoints.ts
+++ src/utils/breakpoints.ts
@@ -1,5 +1,5 @@
-export const LG_BREAKPOINT = 992;
+export const LG_BREAKPOINT = 1024;
 export const MD_BREAKPOINT = 768;
-export const SM_BREAKPOINT = 576;
-export const XL_BREAKPOINT = 1200;
-export const XXL_BREAKPOINT = 1400;
+export const SM_BREAKPOINT = 512;
+export const XL_BREAKPOINT = 1280;
+export const XXL_BREAKPOINT = 1440;
