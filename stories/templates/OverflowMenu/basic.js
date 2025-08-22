e2f0df3c0f2dfa5f309487cb4158ec591ed8c5ce
Tong Hau <tonghau_Lee@hive.gov.sg>
2025-08-22 12:24:12 +0800

    feat(overflow menu): update overflow menu to be public component

---
+++ stories/templates/OverflowMenu/basic.js
@@ -0,0 +1,22 @@
+import { html } from "lit-html";
+import { ifDefined } from "lit/directives/if-defined.js";
+
+export const Template = args => html`
+  <sgds-overflow-menu size=${ifDefined(args.size)}>
+    <sgds-dropdown-item>View</sgds-dropdown-item>
+    <sgds-dropdown-item>Edit</sgds-dropdown-item>
+    <sgds-dropdown-item>Delete</sgds-dropdown-item>
+  </sgds-overflow-menu>
+`;
+
+export const args = {
+  size: "md"
+};
+
+export const parameters = {
+  docs: {
+    story: {
+      height: "300px"
+    }
+  }
+};
