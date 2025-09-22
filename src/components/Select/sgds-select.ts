9bec3fc60967c2b8f47732a1cc8b92c7fa9871a3
clukhei <kheiboardwarrior@gmail.com>
2025-09-22 15:56:42 +0800

    build(*): update react event names to return if name not exist

--- src/components/Select/sgds-select.ts
+++ src/components/Select/sgds-select.ts
@@ -14,6 +14,7 @@ import SelectItem from "./select-item";
  * @event sgds-change - Emitted when the select value changes.
  * @event sgds-focus -  Emitted when user input is focused.
  * @event sgds-blur -  Emitted when user input is blurred.
+ * @event change -  Emitted when the select value changes (native event)
  *
  */
 export class SgdsSelect extends SelectElement {
@@ -51,7 +52,7 @@ export class SgdsSelect extends SelectElement {
   @watch("value", { waitUntilFirstUpdate: true })
   async _handleValueChange() {
     // when value change, always emit a change event
-    this.relayNativeEvent(new Event("change", { bubbles: true, composed: true }));
+    // this.relayNativeEvent(new Event("change", { bubbles: true, composed: true }));
     this.emit("sgds-change");
 
     if (this.value) {
