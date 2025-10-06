99dfd0743a02036056b9256cf180bc8b57f4d236
tong.hau <tong.hau@theoptimum.net>
2025-10-06 11:29:32 +0800

    test(close button): fix failing test case

--- test/close-button.test.ts
+++ test/close-button.test.ts
@@ -11,26 +11,11 @@ describe("<sgds-close-button>", () => {
     assert.shadowDom.equal(
       el,
       `
-      <button class="btn-close btn-close-md" aria-label="Close button">
+      <button class="btn-close" aria-label="Close button">
         <sgds-icon name="cross" size="md"></sgds-icon>
       </button>
       `
     );
-  });
-  it("variant=dark adds a .btn-close-dark ", async () => {
-    const el = await fixture(html`<sgds-close-button variant="dark"></sgds-close-button>`);
-    const btnClose = el.shadowRoot?.querySelector(".btn-close");
-    expect(btnClose?.classList.value).to.contain("btn-close-dark");
-  });
-  it("variant=light adds a .btn-close-dark ", async () => {
-    const el = await fixture(html`<sgds-close-button variant="light"></sgds-close-button>`);
-    const btnClose = el.shadowRoot?.querySelector(".btn-close");
-    expect(btnClose?.classList.value).to.contain("btn-close-light");
-  });
-  it("size=sm adds a .btn-close-sm ", async () => {
-    const el = await fixture(html`<sgds-close-button size="sm"></sgds-close-button>`);
-    const btnClose = el.shadowRoot?.querySelector(".btn-close");
-    expect(btnClose?.classList.value).to.contain("btn-close-sm");
   });
   it("emits click when clicked on", async () => {
     const el = await fixture<SgdsCloseButton>(html`<sgds-close-button></sgds-close-button>`);
