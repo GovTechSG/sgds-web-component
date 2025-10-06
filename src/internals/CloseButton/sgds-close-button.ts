8992b4dccb83d601e51ac6226d77120481cb06d8
tong.hau <tong.hau@theoptimum.net>
2025-10-06 16:33:11 +0800

    chore(close button): remove unused import

--- src/internals/CloseButton/sgds-close-button.ts
+++ src/internals/CloseButton/sgds-close-button.ts
@@ -1,6 +1,5 @@
 import { html } from "lit";
 import { property } from "lit/decorators.js";
-import { classMap } from "lit/directives/class-map.js";
 import { ifDefined } from "lit/directives/if-defined.js";
 import SgdsElement from "../../base/sgds-element";
 import closeButtonStyles from "./close-button.css";
