183d285efabdb761ab83e8c0446a7cfcec1f41aa
Tong Hau <lee_tong_hau@hive.tech.gov.sg>
2025-11-12 13:45:54 +0800

    test(skeleton): add skeleton example in nuxt cypress app

---
+++ cypress/apps/nuxt-app/components/Skeleton.vue
@@ -0,0 +1,53 @@
+<script lang="ts" setup>
+import "@govtechsg/sgds-web-component/components/Skeleton/index.js"
+import "@govtechsg/sgds-web-component/components/ThumbnailCard/index.js"
+</script>
+
+<template>
+  <div class="sgds-container">
+    <div class="sgds-grid">
+      <div class="col-4">
+        <sgds-thumbnail-card>
+          <!-- skeleton for product card image -->
+          <sgds-skeleton
+            slot="thumbnail"
+            sheen
+            width="var(--sgds-dimension-128)"
+            height="var(--sgds-dimension-64)"
+          />
+
+          <!-- skeleton for product card name -->
+          <sgds-skeleton
+            slot="title"
+            sheen
+            width="var(--sgds-dimension-224)"
+            height="var(--sgds-dimension-32)"
+          />
+
+          <!-- skeleton for product card status -->
+          <sgds-skeleton
+            sheen
+            width="var(--sgds-dimension-96)"
+            height="var(--sgds-dimension-24)"
+          />
+
+          <sgds-skeleton
+            slot="description"
+            :rows="3"
+            sheen
+            width="264px"
+            height="76px"
+          />
+
+          <div slot="lower">
+            <sgds-skeleton
+              sheen
+              width="var(--sgds-dimension-96)"
+              height="var(--sgds-dimension-24)"
+            />
+          </div>
+        </sgds-thumbnail-card>
+      </div>
+    </div>
+  </div>
+</template>
