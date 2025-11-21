ea263f064a3137aba422a297fa2108df529dd5fa
clukhei <kheiboardwarrior@gmail.com>
2025-11-22 00:26:20 +0800

    feat(SystemBanner): show more when line has clamped

--- src/components/SystemBanner/sgds-system-banner-item.ts
+++ src/components/SystemBanner/sgds-system-banner-item.ts
@@ -1,6 +1,8 @@
-import { html } from "lit";
+import { html, nothing } from "lit";
 import SgdsElement from "../../base/sgds-element";
 import alertBannerItemStyles from "./system-banner-item.css";
+import { state } from "lit/decorators.js";
+import { classMap } from "lit/directives/class-map.js";
 export type AlertBannerVariant = "info" | "danger" | "warning" | "neutral";
 
 /**
@@ -13,13 +15,45 @@ export type AlertBannerVariant = "info" | "danger" | "warning" | "neutral";
 export class SgdsSystemBannerItem extends SgdsElement {
   static styles = [...SgdsElement.styles, alertBannerItemStyles];
 
+  @state() clamped = false;
+  _resizeObserver: ResizeObserver;
+  async firstUpdated(_changedProperties) {
+    super.firstUpdated(_changedProperties);
+    await this.updateComplete
+    this._clampCheck();
+
+    // Watch resizing for dynamic layout changes
+    this._resizeObserver = new ResizeObserver(() => this._clampCheck());
+    this._resizeObserver.observe(
+      this.shadowRoot.querySelector('.message')
+    );
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    if (this._resizeObserver) this._resizeObserver.disconnect();
+  }
+   private _clampCheck() {
+    const textEl = this.shadowRoot.querySelector('.message');
+
+    requestAnimationFrame(() => {
+      this.clamped = textEl.scrollHeight > textEl.clientHeight;
+    });
+  } 
+
+  private _handleShowMoreClick() {
+   this.emit('sgds-show-more');
+  }
   render() {
     return html`
       <div class="banner-item">
         <slot name="icon"></slot>
         <div class="banner-item__message_and__action">
-          <div class="message">
-            <slot></slot>
+          <div class="clamped-container">
+            <div class=${classMap({ message: true, truncated: this.clamped })}>
+              <slot></slot>
+            </div>
+           ${this.clamped ? html`<a class="inline-link" @click="${this._handleShowMoreClick}">show more</a>` : nothing}
           </div>
           <div class="action">
             <slot name="action"></slot>
