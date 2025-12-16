4f2915bc5d61771308c834499fa1fa8c9d21d9f0
clukhei <kheiboardwarrior@gmail.com>
2025-12-16 23:23:39 +0800

    docs(Link): update storybook with more examples

--- stories/templates/Link/basic.js
+++ stories/templates/Link/basic.js
@@ -2,7 +2,12 @@ import { html } from "lit-html";
 import { ifDefined } from "lit/directives/if-defined.js";
 
 export const Template = args => html`
-  <sgds-link size=${ifDefined(args.size)} variant=${ifDefined(args.variant)}>
+  <sgds-link
+    size=${ifDefined(args.size)}
+    variant=${ifDefined(args.variant)}
+    ?active=${args.active}
+    ?disabled=${args.disabled}
+  >
     <a href="#">Link</a>
   </sgds-link>
 `;
