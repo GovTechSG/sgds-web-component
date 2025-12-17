e42b36cf017f2aee74c440b13b99dffff0810b82
clukhei <kheiboardwarrior@gmail.com>
2025-12-17 14:22:07 +0800

    refactor(Footer,Breadcrumb): patch link dependecies

--- stories/templates/Footer/basic.js
+++ stories/templates/Footer/basic.js
@@ -13,52 +13,44 @@ export const Template = args => {
         termsOfUseHref=${ifDefined(args.termsOfUseHref)}
         copyrightLiner=${ifDefined(args.copyrightLiner)}
       ></sgds-footer>
-      <sgds-footer
-        lastUpdatedDate=${ifDefined(args.lastUpdatedDate)}
-        contactHref=${ifDefined(args.contactHref)}
-        feedbackHref=${ifDefined(args.feedbackHref)}
-        faqHref=${ifDefined(args.faqHref)}
-        privacyHref=${ifDefined(args.privacyHref)}
-        termsOfUseHref=${ifDefined(args.termsOfUseHref)}
-        copyrightLiner=${ifDefined(args.copyrightLiner)}
-      >
+      <sgds-footer contacthref="https://form.gov.sg/" feedbackhref="https://form.gov.sg/" faqhref="https://form.gov.sg/" privacyhref="https://www.designsystem.tech.gov.sg/privacy/" termsofusehref="https://www.designsystem.tech.gov.sg/terms-of-use/">
         <h2 slot="title">Name of portal/digital service</h2>
         <p slot="description">Description of portal/digital service</p>
         <sgds-footer-item slot="items">
           <div slot="title">Application Guidelines</div>
-          <a href="/application-guidelines/lorem-ipsum-one/second-level-a/">hello world</a>
-          <a href="/application-guidelines/lorem-ipsum-one/part-A/">Second Level B</a>
-          <a href="/application-guidelines/lorem-ipsum-three/">Lorem Ipsum Three</a>
+          <sgds-link><a href="/application-guidelines/lorem-ipsum-one/second-level-a/">hello world</a></sgds-link>
+           <sgds-link><a href="/application-guidelines/lorem-ipsum-one/part-A/">Second Level B</a></sgds-link>
+           <sgds-link><a href="/application-guidelines/lorem-ipsum-three/">Lorem Ipsum Three</a></sgds-link>
         </sgds-footer-item>
         <sgds-footer-item slot="items">
           <div slot="title">Legislation</div>
-          <a href="#">Legislation</a>
-          <a href="https://en.wikipedia.org/wiki/Year" target="_blank">External Link One</a>
-          <a href="https://en.wikipedia.org/wiki/Spring_(season)" target="_blank">External Link Two</a>
+          <sgds-link><a href="#">Legislation</a></sgds-link>
+          <sgds-link><a href="https://en.wikipedia.org/wiki/Year" target="_blank">External Link One</a></sgds-link>
+          <sgds-link><a href="https://en.wikipedia.org/wiki/Spring_(season)" target="_blank">External Link Two</a></sgds-link>
         </sgds-footer-item>
         <sgds-footer-item slot="items">
           <div slot="title">Resources</div>
-          <a href="/resource_room/">All</a>
-          <a href="/resource_room/forms-and-templates/" target="_blank">Forms and Templates</a>
-          <a href="/resource_room/guides/" target="_blank">Guides</a>
+          <sgds-link><a href="/resource_room/">All</a></sgds-link>
+          <sgds-link><a href="/resource_room/forms-and-templates/" target="_blank">Forms and Templates</a></sgds-link>
+          <sgds-link><a href="/resource_room/guides/" target="_blank">Guides</a></sgds-link>
         </sgds-footer-item>
         <sgds-footer-item slot="items">
           <div slot="title">Resources</div>
-          <a href="/resource_room/">All</a>
-          <a href="/resource_room/forms-and-templates/" target="_blank">Forms and Templates</a>
-          <a href="/resource_room/guides/" target="_blank">Guides</a>
+          <sgds-link><a href="/resource_room/">All</a></sgds-link>
+          <sgds-link><a href="/resource_room/forms-and-templates/" target="_blank">Forms and Templates</a></sgds-link>
+          <sgds-link><a href="/resource_room/guides/" target="_blank">Guides</a></sgds-link>
         </sgds-footer-item>
         <sgds-footer-item slot="items">
           <div slot="title">Resources</div>
-          <a href="/resource_room/">All</a>
-          <a href="/resource_room/forms-and-templates/" target="_blank">Forms and Templates</a>
-          <a href="/resource_room/guides/" target="_blank">Guides</a>
+          <sgds-link><a href="/resource_room/">All</a></sgds-link>
+          <sgds-link><a href="/resource_room/forms-and-templates/" target="_blank">Forms and Templates</a></sgds-link>
+          <sgds-link><a href="/resource_room/guides/" target="_blank">Guides</a></sgds-link>
         </sgds-footer-item>
         <sgds-footer-item slot="items">
           <div slot="title">Resources</div>
-          <a href="/resource_room/">All</a>
-          <a href="/resource_room/forms-and-templates/" target="_blank">Forms and Templates</a>
-          <a href="/resource_room/guides/" target="_blank">Guides</a>
+          <sgds-link><a href="/resource_room/">All</a></sgds-link>
+          <sgds-link><a href="/resource_room/forms-and-templates/" target="_blank">Forms and Templates</a></sgds-link>
+          <sgds-link><a href="/resource_room/guides/" target="_blank">Guides</a></sgds-link>
         </sgds-footer-item>
 
         <a slot="social-media" href="https://www.facebook.com">
--- test/footer.test.ts
+++ test/footer.test.ts
@@ -1,7 +1,7 @@
 import "./sgds-web-component";
 import { fixture, assert, expect } from "@open-wc/testing";
 import { html } from "lit";
-import { SgdsFooter } from "../src/components";
+import { SgdsFooter, SgdsLink } from "../src/components";
 
 describe("footer", () => {
   it("renders with default values", async () => {
@@ -25,27 +25,27 @@ describe("footer", () => {
             <div class="footer-mandatory-links">
               <ul>
                 <li>
-                  <sgds-link size="sm" variant="primary">
+                  <sgds-link size="sm" variant="primary" tone="fixed-light">
                     <a href="#" tabindex="0">Contact</a>
                   </sgds-link>
                 </li>
                 <li>
-                  <sgds-link size="sm" variant="primary">
+                  <sgds-link size="sm" variant="primary" tone="fixed-light">
                     <a href="#" tabindex="0">Feedback</a>
                   </sgds-link>
                 </li>
                 <li>
-                  <sgds-link size="sm" variant="primary">
+                  <sgds-link size="sm" variant="primary" tone="fixed-light">
                     <a href="#" tabindex="0">FAQ</a>
                   </sgds-link>
                 </li>
                 <li>
-                  <sgds-link size="sm" variant="primary">
+                  <sgds-link size="sm" variant="primary" tone="fixed-light">
                     <a href="#" tabindex="0">Sitemap</a>
                   </sgds-link>
                 </li>
                 <li>
-                  <sgds-link size="sm" variant="primary">
+                  <sgds-link size="sm" variant="primary" tone="fixed-light">
                     <a
                       href="https://tech.gov.sg/report_vulnerability"
                       rel="noopener noreferrer"
@@ -56,12 +56,12 @@ describe("footer", () => {
                   </sgds-link>
                 </li>
                 <li>
-                  <sgds-link size="sm" variant="primary">
+                  <sgds-link size="sm" variant="primary" tone="fixed-light">
                     <a href="#" tabindex="0">Privacy Statement</a>
                   </sgds-link>
                 </li>
                 <li>
-                  <sgds-link size="sm" variant="primary">
+                  <sgds-link size="sm" variant="primary" tone="fixed-light">
                     <a href="#" tabindex="0">Terms of use</a>
                   </sgds-link>
                 </li>
@@ -99,27 +99,27 @@ describe("footer", () => {
             <div class="footer-mandatory-links">
               <ul>
                 <li>
-                  <sgds-link size="sm" variant="primary">
+                  <sgds-link size="sm" variant="primary" tone="fixed-light">
                     <a href="#" tabindex="0">Contact</a>
                   </sgds-link>
                 </li>
                 <li>
-                  <sgds-link size="sm" variant="primary">
+                  <sgds-link size="sm" variant="primary" tone="fixed-light">
                     <a href="#" tabindex="0">Feedback</a>
                   </sgds-link>
                 </li>
                 <li>
-                  <sgds-link size="sm" variant="primary">
+                  <sgds-link size="sm" variant="primary" tone="fixed-light">
                     <a href="#" tabindex="0">FAQ</a>
                   </sgds-link>
                 </li>
                 <li>
-                  <sgds-link size="sm" variant="primary">
+                  <sgds-link size="sm" variant="primary" tone="fixed-light">
                     <a href="#" tabindex="0">Sitemap</a>
                   </sgds-link>
                 </li>
                 <li>
-                  <sgds-link size="sm" variant="primary">
+                  <sgds-link size="sm" variant="primary" tone="fixed-light">
                     <a
                       href="https://tech.gov.sg/report_vulnerability"
                       rel="noopener noreferrer"
@@ -130,12 +130,12 @@ describe("footer", () => {
                   </sgds-link>
                 </li>
                 <li>
-                  <sgds-link size="sm" variant="primary">
+                  <sgds-link size="sm" variant="primary" tone="fixed-light">
                     <a href="#" tabindex="0">Privacy Statement</a>
                   </sgds-link>
                 </li>
                 <li>
-                  <sgds-link size="sm" variant="primary">
+                  <sgds-link size="sm" variant="primary" tone="fixed-light">
                     <a href="#" tabindex="0">Terms of use</a>
                   </sgds-link>
                 </li>
@@ -194,9 +194,9 @@ describe("SgdsFooterItem", () => {
   it("renders with default structure", async () => {
     const el = await fixture<SgdsFooter>(html` <sgds-footer-item>
       <div slot="title">Application Guidelines</div>
-      <a href="/application-guidelines/lorem-ipsum-one/second-level-a/">hello world</a>
-      <a href="/application-guidelines/lorem-ipsum-one/part-A/">Second Level B</a>
-      <a href="/application-guidelines/lorem-ipsum-three/">Lorem Ipsum Three</a>
+      <sgds-link><a href="/application-guidelines/lorem-ipsum-one/second-level-a/">hello world</a></sgds-link>
+      <sgds-link><a href="/application-guidelines/lorem-ipsum-one/part-A/">Second Level B</a></sgds-link>
+      <sgds-link><a href="/application-guidelines/lorem-ipsum-three/">Lorem Ipsum Three</a></sgds-link>
     </sgds-footer-item>`);
     const titleSlot = el.shadowRoot?.querySelector('slot[name="title"]');
     const defaultSlot = el.shadowRoot?.querySelector("slot:not([name])");
@@ -204,4 +204,17 @@ describe("SgdsFooterItem", () => {
     expect(titleSlot).to.exist;
     expect(defaultSlot).to.exist;
   });
+  it("slotted sgds-link should have tone=fixed-light and size=sm attributes added", async() => {
+    const el = await fixture<SgdsFooter>(html`<sgds-footer-item>
+      <div slot="title">Application Guidelines</div>
+      <sgds-link><a href="/application-guidelines/lorem-ipsum-one/second-level-a/">hello world</a></sgds-link>
+      <sgds-link><a href="/application-guidelines/lorem-ipsum-one/part-A/">Second Level B</a></sgds-link>
+      <sgds-link><a href="/application-guidelines/lorem-ipsum-three/">Lorem Ipsum Three</a></sgds-link>
+    </sgds-footer-item>`);
+    const links = el.querySelectorAll<SgdsLink>("sgds-link")
+    links.forEach(l => {
+      expect(l.tone).to.equal("fixed-light")
+      expect(l.size).to.equal("sm")
+    })
+  })
 });
