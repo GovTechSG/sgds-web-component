5d8b9cddfa7dc275cff96bf7078f726afac8a254
clukhei <kheiboardwarrior@gmail.com>
2025-12-18 18:29:36 +0800

    docs(*): storybook add sgds viewport sizes

---
+++ .storybook/modes.ts
@@ -0,0 +1,8 @@
+export const allModes = {
+  xs: {viewport: "xs",},
+  sm: { viewport: "sm",},
+  md: { viewport: "md",},
+  lg: { viewport: "lg",},
+  xl: { viewport: "xl", },
+  "2-xl": {viewport: "2-xl", },
+} as const;
