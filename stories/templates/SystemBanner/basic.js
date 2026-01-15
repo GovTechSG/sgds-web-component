3801a7fe6aabed88f41a213abd5d68ad2f9f0025
clukhei <kheiboardwarrior@gmail.com>
2026-01-15 11:26:48 +0800

    docs(SystemBanner): remove div element in documentation

--- stories/templates/SystemBanner/basic.js
+++ stories/templates/SystemBanner/basic.js
@@ -4,21 +4,17 @@ import { ifDefined } from "lit/directives/if-defined.js";
 export const Template = args => html`<sgds-system-banner ?dismissible=${args.dismissible} ?show=${args.show}>
   <sgds-system-banner-item>
     <sgds-icon slot="icon" name="placeholder" size="md"></sgds-icon>
-    <div>
-      Etiam suscipit nisi eget porta cursus. Ut sit amet felis aliquet, pellentesque mi at, vulputate nunc. Vivamus ac
-      facilisis tellus.
-    </div>
+    Etiam suscipit nisi eget porta cursus. Ut sit amet felis aliquet, pellentesque mi at, vulputate nunc. Vivamus ac
+    facilisis tellus.
     <sgds-link size="sm" variant="light" slot="action"
       ><a href="#">Action link<sgds-icon name="arrow-right"></sgds-icon></a
     ></sgds-link>
   </sgds-system-banner-item>
   <sgds-system-banner-item>
     <sgds-icon slot="icon" name="placeholder" size="md"></sgds-icon>
-    <div>
-      Etiam suscipit nisi eget porta cursus. Ut sit amet felis aliquet, pellentesque mi at, vulputate nunc. Vivamus ac
-      facilisis tellus. Maecenas ac libero scelerisque tellus maximus accumsan a vehicula arcu. Aenean quis leo gravida,
-      congue sapien eu, rhoncus
-    </div>
+    Etiam suscipit nisi eget porta cursus. Ut sit amet felis aliquet, pellentesque mi at, vulputate nunc. Vivamus ac
+    facilisis tellus. Maecenas ac libero scelerisque tellus maximus accumsan a vehicula arcu. Aenean quis leo gravida,
+    congue sapien eu, rhoncus
     <sgds-button href="#" slot="action" size="sm" tone="fixed-light" variant="outline">
       Action button
       <sgds-icon name="arrow-right" slot="rightIcon" size="md"></sgds-icon>
@@ -26,11 +22,9 @@ export const Template = args => html`<sgds-system-banner ?dismissible=${args.dis
   </sgds-system-banner-item>
   <sgds-system-banner-item>
     <sgds-icon slot="icon" name="placeholder" size="md"></sgds-icon>
-    <div>
-      Etiam suscipit nisi eget porta cursus. Ut sit amet felis aliquet, pellentesque mi at, vulputate nunc. Vivamus ac
-      facilisis tellus. Maecenas ac libero scelerisque tellus maximus accumsan a vehicula arcu. Aenean quis leo gravida,
-      congue sapien eu, rhoncus
-    </div>
+    Etiam suscipit nisi eget porta cursus. Ut sit amet felis aliquet, pellentesque mi at, vulputate nunc. Vivamus ac
+    facilisis tellus. Maecenas ac libero scelerisque tellus maximus accumsan a vehicula arcu. Aenean quis leo gravida,
+    congue sapien eu, rhoncus
     <sgds-link size="sm" variant="light" slot="action"
       ><a href="#">Action link<sgds-icon name="arrow-right" size="md"></sgds-icon></a
     ></sgds-link>
