c38b85ccff9d50a04e87222d19879b45d24eae6b
clukhei <kheiboardwarrior@gmail.com>
2026-01-30 15:23:35 +0800

    feat(SystemBanner): noClampAction prop to make show more action optional

--- src/components/SystemBanner/system-banner-context.ts
+++ src/components/SystemBanner/system-banner-context.ts
@@ -1,3 +1,5 @@
 import { createContext } from "@lit/context";
 
+export const NoClampActionContext = createContext<boolean>("no-clamp-action-context");
+
 export const SystemBannerChildCountContext = createContext<boolean>("system-banner-children-count-context");
