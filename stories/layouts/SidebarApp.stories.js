6084a073fe1fcfa7602316e3e59e8c0e6e974a21
clukhei <kheiboardwarrior@gmail.com>
2026-02-02 13:59:04 +0800

    chore(*): example using pure css

--- stories/layouts/SidebarApp.stories.js
+++ stories/layouts/SidebarApp.stories.js
@@ -35,3 +35,86 @@ export const ApplicationLayoutWithSidebar = {
   parameters: { layout: "fullscreen" },
   tags: []
 };
+
+const CSSTemplate = () => {
+  return html`
+    <style>
+      .sticky-top {
+        position: sticky;
+        top: 0;
+      }
+
+      .flex-container {
+        display: flex;
+        flex-direction: row;
+      }
+
+      .sidebar {
+        position: sticky;
+        background-color: var(--sgds-bg-translucent-subtle);
+        height: calc(100vh - 108px);
+        overflow-y: scroll;
+        top: 6.75rem;
+        width: 4.5rem;
+      }
+
+      @media (min-width: 768px) {
+        .sidebar {
+          width: 17rem;
+        }
+      }
+
+      .sidebar-component {
+        background-color: var(--sgds-bg-translucent-subtle);
+        height: 1000px;
+        width: 100%;
+      }
+
+      .main-content {
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+      }
+
+      .sgds-container-sidebar {
+        padding-top: var(--sgds-padding-2-xl);
+        padding-bottom: var(--sgds-padding-2-xl);
+      }
+
+      .content {
+        background-color: var(--sgds-bg-translucent-subtle);
+        height: 1000px;
+        width: 100%;
+      }
+    </style>
+
+    <div>
+      <div class="sticky-top">
+        <sgds-masthead fluid></sgds-masthead>
+        <sgds-mainnav fluid>
+          <strong slot="brand">SGDS</strong>
+          <strong slot="end">End</strong>
+        </sgds-mainnav>
+      </div>
+      <div class="flex-container">
+        <div class="sidebar">
+          <div id="sidebar-component" class="sidebar-component">Place SideBar here</div>
+        </div>
+        <div class="main-content">
+          <div class="sgds-container-sidebar">
+            <div id="content" class="content">Place content here</div>
+          </div>
+          <sgds-footer></sgds-footer>
+        </div>
+      </div>
+    </div>
+  `;
+};
+
+export const ApplicationLayoutWithSidebarCSS = {
+  render: CSSTemplate.bind({}),
+  name: "Application layout with sidebar (CSS)",
+  args: {},
+  parameters: { layout: "fullscreen" },
+  tags: []
+};
