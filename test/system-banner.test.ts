6a6e5990bfc8b974d6e813e594610ea1e0d4d90c
clukhei <kheiboardwarrior@gmail.com>
2026-03-02 11:58:18 +0800

    fix(SystemBanner): raise media query screen min width to laptop 1024px

--- test/system-banner.test.ts
+++ test/system-banner.test.ts
@@ -267,7 +267,11 @@ describe("<sgds-system-banner>", () => {
     const el = await fixture<SgdsSystemBannerItem>(html`<sgds-system-banner-item noClampAction>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet
       consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      Quisquam, voluptatum.
+      Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum
+      dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+      voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit
+      amet consectetur adipisicing elit. Quisquam, voluptatum.
     </sgds-system-banner-item>`);
 
     await el.updateComplete;
@@ -278,16 +282,20 @@ describe("<sgds-system-banner>", () => {
   it("when noClampAction is false, show more link appears for long text", async () => {
     const el = await fixture<SgdsSystemBanner>(html`
       <sgds-system-banner-item>
-        1 Etiam suscipit nisi eget porta cursus. Ut sit amet felis aliquet, pellentesque mi at, vulputate nunc. Vivamus
-        ac facilisis tellus. Maecenas ac libero scelerisque tellus maximus accumsan a vehicula arcu. Aenean quis leo
-        gravida, congue sapien eu, rhoncus Maecenas ac libero scelerisque tellus maximus accumsan a vehicula arcu.
-        Aenean quis leo gravida, congue sapien eu, rhoncus
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+        voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit
+        amet consectetur adipisicing elit. Quisquam, voluptatum.
       </sgds-system-banner-item>
     `);
 
     await el.updateComplete;
     const messageDiv = el.shadowRoot?.querySelector(".message");
     await waitUntil(() => messageDiv?.classList.contains("truncated") === true, "Message div did not get truncated");
+
     expect(messageDiv?.classList.contains("truncated")).to.be.true;
     const showMore = el.shadowRoot?.querySelector(".show-more");
     expect(showMore).to.exist;
