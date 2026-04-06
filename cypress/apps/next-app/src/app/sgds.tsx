d091215fb8b5e69f2aecff839170d31ea0e4b154
clukhei <kheiboardwarrior@gmail.com>
2026-04-06 16:53:40 +0800

    chore(*): add scope element polyfill

--- cypress/apps/next-app/src/app/sgds.tsx
+++ cypress/apps/next-app/src/app/sgds.tsx
@@ -5,6 +5,7 @@ import { useEffect } from 'react';
 const SgdsLibraryLoader = ({ nonce }: { nonce?: string }) => {
   useEffect(() => {
     (async () => {
+      await import('@webcomponents/scoped-custom-element-registry');
       await import('@govtechsg/sgds-web-component');
     })();
   }, []);
