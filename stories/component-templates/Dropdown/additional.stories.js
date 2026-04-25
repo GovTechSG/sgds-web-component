c454f3a20d7d2b4fb0eb900e4956d0185e3d41a7
Jesmond Chia <jesmond_chia_from.scientec@tech.gov.sg>
2026-04-25 19:19:37 +0800

    feat(dropdown): add SgdsDropdownItem customisation story

--- stories/component-templates/Dropdown/additional.stories.js
+++ stories/component-templates/Dropdown/additional.stories.js
@@ -79,3 +79,189 @@ export const SgdsSelectEvent = {
   },
   tags: ["!dev"]
 };
+
+const SgdsSelectDropdownItemTemplate = args => {
+  return html`
+    <sgds-dropdown close="outside">
+      <sgds-button slot="toggler" role="button" variant="primary" tone="brand">
+        Dropdown
+        <sgds-icon name="chevron-down" slot="rightIcon"></sgds-icon>
+      </sgds-button>
+      <sgds-dropdown-item>
+        <div
+          style="
+          flex-grow: 1;
+          align-items: center;
+        "
+        >
+          <sgds-icon name="placeholder" size="2-xl"></sgds-icon>
+          <div
+            style="
+            display: flex;
+            flex-direction: column;
+            gap: var(--sgds-text-gap-2-xs);
+          "
+          >
+            <span
+              style="
+              font-size: var(--sgds-font-size-label-sm);
+              line-height: var(--sgds-line-height-2-xs);
+              font-weight: var(--sgds-font-weight-regular);
+              letter-spacing: var(--sgds-letter-spacing-normal);
+            "
+              >Label</span
+            >
+            <span
+              style="
+              font-size: var(--sgds-font-size-label-xs);
+              line-height: var(--sgds-line-height-3-xs);
+              font-weight: var(--sgds-font-weight-regular);
+              letter-spacing: var(--sgds-letter-spacing-normal);
+              color: var(--sgds-color-subtle);
+            "
+              >Secondary text</span
+            >
+          </div>
+          <sgds-icon name="placeholder"></sgds-icon>
+        </div>
+      </sgds-dropdown-item>
+      <sgds-dropdown-item>
+        <div
+          style="
+          flex-grow: 1;
+          align-items: center;
+        "
+        >
+          <sgds-icon name="placeholder"></sgds-icon>
+          <span
+            style="
+            font-size: var(--sgds-font-size-label-sm);
+            line-height: var(--sgds-line-height-2-xs);
+            font-weight: var(--sgds-font-weight-regular);
+            letter-spacing: var(--sgds-letter-spacing-normal);
+            flex-grow: 1;
+          "
+            >Label</span
+          >
+          <span
+            style="
+            font-size: var(--sgds-font-size-label-xs);
+            line-height: var(--sgds-line-height-3-xs);
+            font-weight: var(--sgds-font-weight-regular);
+            letter-spacing: var(--sgds-letter-spacing-normal);
+            color: var(--sgds-color-subtle);
+          "
+            >Secondary text</span
+          >
+        </div>
+      </sgds-dropdown-item>
+      <sgds-dropdown-item>
+        <div
+          style="
+          flex-grow: 1;
+          align-items: center;
+          justify-content: space-between;
+        "
+        >
+          <div
+            style="
+            display: flex;
+            flex-direction: column;
+            gap: var(--sgds-text-gap-2-xs);
+          "
+          >
+            <span
+              style="
+              font-size: var(--sgds-font-size-label-sm);
+              line-height: var(--sgds-line-height-2-xs);
+              font-weight: var(--sgds-font-weight-regular);
+              letter-spacing: var(--sgds-letter-spacing-normal);
+            "
+              >Label</span
+            >
+            <span
+              style="
+              font-size: var(--sgds-font-size-label-xs);
+              line-height: var(--sgds-line-height-3-xs);
+              font-weight: var(--sgds-font-weight-regular);
+              letter-spacing: var(--sgds-letter-spacing-normal);
+              color: var(--sgds-color-subtle);
+            "
+              >Secondary text</span
+            >
+          </div>
+          <sgds-badge variant="white" outlined>Badge</sgds-badge>
+        </div>
+      </sgds-dropdown-item>
+      <sgds-dropdown-item>
+        <div
+          style="
+          flex-grow: 1;
+          align-items: center;
+        "
+        >
+          <sgds-icon name="placeholder"></sgds-icon>
+          <span
+            style="
+            font-size: var(--sgds-font-size-label-sm);
+            line-height: var(--sgds-line-height-2-xs);
+            font-weight: var(--sgds-font-weight-regular);
+            letter-spacing: var(--sgds-letter-spacing-normal);
+            flex-grow: 1;
+          "
+            >Label</span
+          >
+          <sgds-switch size="sm"></sgds-switch>
+        </div>
+      </sgds-dropdown-item>
+      <sgds-dropdown-item>
+        <div
+          style="
+          flex-grow: 1;
+          align-items: center;
+          justify-content: space-between;
+        "
+        >
+          <div
+            style="
+            display: flex;
+            flex-direction: column;
+            gap: var(--sgds-text-gap-2-xs);
+          "
+          >
+            <span
+              style="
+              font-size: var(--sgds-font-size-label-sm);
+              line-height: var(--sgds-line-height-2-xs);
+              font-weight: var(--sgds-font-weight-regular);
+              letter-spacing: var(--sgds-letter-spacing-normal);
+            "
+              >Label</span
+            >
+            <span
+              style="
+              font-size: var(--sgds-font-size-label-xs);
+              line-height: var(--sgds-line-height-3-xs);
+              font-weight: var(--sgds-font-weight-regular);
+              letter-spacing: var(--sgds-letter-spacing-normal);
+              color: var(--sgds-color-subtle);
+            "
+              >Secondary text</span
+            >
+          </div>
+          <sgds-button variant="outline" tone="neutral" size="xs">Action</sgds-button>
+        </div>
+      </sgds-dropdown-item>
+    </sgds-dropdown>
+  `;
+};
+
+export const SgdsSelectDropdownItem = {
+  render: SgdsSelectDropdownItemTemplate.bind({}),
+  name: "sgds-dropdown-item customisation",
+  args: {},
+  parameters: {
+    chromatic: { disableSnapshot: true }
+  },
+  tags: ["!dev"]
+};
