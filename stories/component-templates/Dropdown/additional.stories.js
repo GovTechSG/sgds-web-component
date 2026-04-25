ecc975037695848af54fe33ee2dde1058acf6aa8
Jesmond Chia <jesmond_chia_from.scientec@tech.gov.sg>
2026-04-25 19:19:37 +0800

    feat(dropdown): add SgdsDropdownItem customisation story

--- stories/component-templates/Dropdown/additional.stories.js
+++ stories/component-templates/Dropdown/additional.stories.js
@@ -79,3 +79,78 @@ export const SgdsSelectEvent = {
   },
   tags: ["!dev"]
 };
+
+const SgdsSelectDropdownItemTemplate = args => {
+  return html`
+    <sgds-dropdown close="outside">
+      <sgds-button slot="toggler" role="button" variant="primary" tone="brand">
+        Dropdown
+        <sgds-icon name="chevron-down" slot="rightIcon"></sgds-icon>
+      </sgds-button>
+      <sgds-dropdown-item>
+        <div class="sgds:grow sgds:items-center">
+          <sgds-icon name="placeholder" size="2-xl"></sgds-icon>
+          <div class="sgds:flex sgds:flex-col sgds:gap-text-2-xs">
+            <span class="sgds:text-label-sm sgds:leading-2-xs sgds:font-regular sgds:tracking-normal">Label</span>
+            <span class="sgds:text-label-xs sgds:leading-3-xs sgds:font-regular sgds:tracking-normal sgds:text-subtle"
+              >Secondary text</span
+            >
+          </div>
+          <sgds-icon name="placeholder"></sgds-icon>
+        </div>
+      </sgds-dropdown-item>
+      <sgds-dropdown-item>
+        <div class="sgds:grow sgds:items-center">
+          <sgds-icon name="placeholder"></sgds-icon>
+          <span class="sgds:text-label-sm sgds:leading-2-xs sgds:font-regular sgds:tracking-normal sgds:grow"
+            >Label</span
+          >
+          <span class="sgds:text-label-xs sgds:leading-3-xs sgds:font-regular sgds:tracking-normal sgds:text-subtle"
+            >Secondary text</span
+          >
+        </div>
+      </sgds-dropdown-item>
+      <sgds-dropdown-item>
+        <div class="sgds:grow sgds:items-center sgds:justify-between">
+          <div class="sgds:flex sgds:flex-col sgds:gap-text-2-xs">
+            <span class="sgds:text-label-sm sgds:leading-2-xs sgds:font-regular sgds:tracking-normal">Label</span>
+            <span class="sgds:text-label-xs sgds:leading-3-xs sgds:font-regular sgds:tracking-normal sgds:text-subtle"
+              >Secondary text</span
+            >
+          </div>
+          <sgds-badge variant="white" outlined>Badge</sgds-badge>
+        </div>
+      </sgds-dropdown-item>
+      <sgds-dropdown-item>
+        <div class="sgds:grow sgds:items-center">
+          <sgds-icon name="placeholder"></sgds-icon>
+          <span class="sgds:text-label-sm sgds:leading-2-xs sgds:font-regular sgds:tracking-normal sgds:grow"
+            >Label</span
+          >
+          <sgds-switch size="sm"></sgds-switch>
+        </div>
+      </sgds-dropdown-item>
+      <sgds-dropdown-item>
+        <div class="sgds:grow sgds:items-center sgds:justify-between">
+          <div class="sgds:flex sgds:flex-col sgds:gap-text-2-xs">
+            <span class="sgds:text-label-sm sgds:leading-2-xs sgds:font-regular sgds:tracking-normal">Label</span>
+            <span class="sgds:text-label-xs sgds:leading-3-xs sgds:font-regular sgds:tracking-normal sgds:text-subtle"
+              >Secondary text</span
+            >
+          </div>
+          <sgds-button variant="outline" tone="neutral" size="xs">Action</sgds-button>
+        </div>
+      </sgds-dropdown-item>
+    </sgds-dropdown>
+  `;
+};
+
+export const SgdsSelectDropdownItem = {
+  render: SgdsSelectDropdownItemTemplate.bind({}),
+  name: "sgds-dropdown-item customisation",
+  args: {},
+  parameters: {
+    chromatic: { disableSnapshot: true }
+  },
+  tags: ["!dev"]
+};
