a1485bcd9dfe6705e91b8a963c08f34d8367111a
clukhei <kheiboardwarrior@gmail.com>
2026-09-14 17:53:08 +0800

    docs(Appnav): adjust gap for the profile account slot in dropdown item

--- stories/component-templates/Appnav/basic.js
+++ stories/component-templates/Appnav/basic.js
@@ -1,6 +1,6 @@
 import { html } from "lit";
 import { ifDefined } from "lit/directives/if-defined.js";
-import { allModes } from "../../../.storybook/modes";
+import { appllModes } from "../../../.storybook/modes";
 
 export const Template = ({ tone, expand, brandHref }) => {
   return html`
